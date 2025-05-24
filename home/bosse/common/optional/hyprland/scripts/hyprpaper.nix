559afacd0bce5f7ea40b4918e64885fbb9ef3c12
bossearch <yudha.bosse@gmail.com>
2025-05-24 13:44:44 +0800

    feat: check internet before run hyprpaper.sh

--- home/bosse/common/optional/hyprland/scripts/hyprpaper.nix
+++ home/bosse/common/optional/hyprland/scripts/hyprpaper.nix
@@ -5,31 +5,29 @@
       #!/usr/bin/env bash
 
       set -e
+      # Check for internet connection
+      if ! ping -c 1 1.1.1.1 >/dev/null 2>&1; then
+        echo "No internet connection. Exiting."
+        exit 1
+      fi
 
       pushd ~/Pictures/gowall >/dev/null || exit
 
       DATE=$(date +%Y%m%d)
 
-      INPUT_WALL=$(find /home/bosse/Pictures/gowall -maxdepth 1 -type f -name "wall-''\${DATE}.*" -printf "%f\n" | head -n 1)
+      INPUT_WALL=$(find . -maxdepth 1 -type f -name "wall-''\${DATE}.*" -printf "%f\n" | head -n 1)
       OUTPUT_WALL="/tmp/hyprpaper.png"
       THEME="tokyo-dark"
-      LOG_FILE="$HOME/hyprpaper.log"
-
-      if [ -f "$LOG_FILE" ]; then
-        rm "$LOG_FILE"
-        echo "$DATE" >> "$LOG_FILE"
-      fi
 
       if [ -f "$OUTPUT_WALL" ]; then
-        rm /tmp/{hyprlock.png,hyprpaper.png}
+        rm -f /tmp/{hyprlock.png,hyprpaper.png}
       fi
 
       if [ ! -f "$INPUT_WALL" ]; then
         echo y | gowall -w
-        echo "downloaded" >> "$LOG_FILE"
       fi
 
-      TODAY_WALL=$(find /home/bosse/Pictures/gowall -maxdepth 1 -type f -name "wall-''\${DATE}-*" -printf "%f\n" | head -n 1)
+      TODAY_WALL=$(find . -maxdepth 1 -type f -name "wall-''\${DATE}-*" -printf "%f\n" | head -n 1)
       # Extract components using regex
       if [[ "$TODAY_WALL" =~ wall-([0-9]{8})-[0-9]{6}\.([a-zA-Z0-9]+)$ ]]; then
         date_part="''\${BASH_REMATCH[1]}"
@@ -37,22 +35,15 @@
         dir=$(dirname "$TODAY_WALL")
         RENAMED_WALL="$dir/wall-''\${date_part}.''\${ext}"
         mv "$TODAY_WALL" "$RENAMED_WALL"
-        echo "renamed" >> "$LOG_FILE"
       fi
 
-      INPUT_WALL=$(find /home/bosse/Pictures/gowall -maxdepth 1 -type f -name "wall-''\${DATE}.*" -printf "%f\n" | head -n 1)
+      INPUT_WALL=$(find . -maxdepth 1 -type f -name "wall-''\${DATE}.*" -printf "%f\n" | head -n 1)
       if [ ! -f "$OUTPUT_WALL" ]; then
         gowall convert "$INPUT_WALL" -t "$THEME" --output "$OUTPUT_WALL"
         cp "$OUTPUT_WALL" /tmp/hyprlock.png
-        echo "converted" >> "$LOG_FILE"
       fi
 
       popd >/dev/null || exit
-
-      if ! pgrep -f "Hyprland" > /dev/null; then
-        Hyprland
-        echo "applied" >> "$LOG_FILE"
-      fi
     '';
   };
 }
--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -6,7 +6,8 @@
     enable = true;
     settings = rec {
       initial_session = {
-        command = ".config/hypr/scripts/hyprpaper.sh >/dev/null";
+        command = "bash -c '.config/hypr/scripts/hyprpaper.sh >/dev/null; Hyprland >/dev/null'";
+        # command = ".config/hypr/scripts/hyprpaper.sh >/dev/null";
         user = "bosse";
       };
       default_session = initial_session;
