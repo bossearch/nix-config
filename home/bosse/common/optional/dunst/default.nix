388b133f9c6a9c30db388863a2f205161d191a47
bossearch <yudha.bosse@gmail.com>
2025-05-25 14:19:50 +0800

    fix: dunst to recognize papirus icon

--- home/bosse/common/optional/dunst/default.nix
+++ home/bosse/common/optional/dunst/default.nix
@@ -1,10 +1,14 @@
-{pkgs,...}: {
+{pkgs, ...}: {
   imports = [
     ./dunstsound.nix
     ./sound.nix
   ];
   services.dunst = {
     enable = true;
+    iconTheme = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
     settings = {
       global = {
         background = "#16161e";
@@ -12,7 +16,7 @@
         monitor = 0;
         follow = "mouse";
         width = "(0,600)";
-        height = 300;
+        height = "(0,300)";
         progress_bar = true;
         progress_bar_height = 25;
         progress_bar_frame_width = 3;
@@ -33,8 +37,8 @@
         line_height = 2;
         markup = "full";
         origin = "top-right";
-        offset = "10x10";
-        format = "<b>%s</b>\n%b";
+        offset = "(10, 10)";
+        format = "<b>%s</b>\\n%b";
         alignment = "left";
         show_age_threshold = 60;
         word_wrap = true;
@@ -45,7 +49,7 @@
         icon_position = "left";
         max_icon_size = 86;
         min_icon_size = 32;
-        icon_theme = "Papirus";
+        icon_theme = "Papirus,papirus";
         enable_recursive_icon_lookup = true;
         sticky_history = true;
         history_length = 20;
@@ -62,11 +66,12 @@
         ignore_dbusclose = true;
         ellipsize = "middle";
       };
-      urgency_low= {
+      urgency_low = {
         timeout = 2;
         background = "#1a1b26";
         foreground = "#c0caf5";
         frame_color = "#c0caf5";
+        icon = "dialog-information";
         script = "~/.config/dunst/dunstsound.sh";
       };
       urgency_normal = {
@@ -74,13 +79,15 @@
         background = "#16161e";
         foreground = "#c0caf5";
         frame_color = "#c0caf5";
+        icon = "dialog-information";
         script = "~/.config/dunst/dunstsound.sh";
       };
-      urgency_critical= {
+      urgency_critical = {
         timeout = 4;
         background = "#292e42";
         foreground = "#db4b4b";
         frame_color = "#db4b4b";
+        icon = "dialog-warning";
         script = "~/.config/dunst/dunstsound.sh";
       };
       zsh = {
