a1ca8ec306cf75ab5529d6e4f1e24dd58321fcf7
bossearch <yudha.bosse@gmail.com>
2025-05-27 02:56:46 +0800

    chore: update tmux theme to recent commit

--- home/bosse/common/core/cli/tmux/mythemux.nix
+++ home/bosse/common/core/cli/tmux/mythemux.nix
@@ -2,7 +2,7 @@
   home.file."mythemux" = {
     source = builtins.fetchGit {
       url = "https://github.com/bossearch/mythemux";
-      rev = "6c43ef579ca725028ae821d17a89e3a5ff798e30";
+      rev = "0b26335e17bb405b754cb3d22ca12f070c23fd20";
     };
     target = ".config/tmux/plugins/mythemux";
   };
