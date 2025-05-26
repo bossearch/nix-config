97102b9c66a7c6e157e331122083ebc078f984d2
bossearch <yudha.bosse@gmail.com>
2025-05-26 22:22:39 +0800

    fix: nixvim treesitter-textobjects repeat key

--- home/bosse/common/core/nvim/plugins/treesitter-textobjects/repeat.nix
+++ home/bosse/common/core/nvim/plugins/treesitter-textobjects/repeat.nix
@@ -1,24 +1,13 @@
 {
-  programs.nixvim = {
-    keymaps = [
-      {
-        key = ";";
-        action.__raw = ''
-          function()
-            require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move()
-          end
-        '';
-        mode = ["n" "x" "o"];
-      }
-      {
-        key = ",";
-        action.__raw = ''
-          function()
-            require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move_opposite()
-          end
-        '';
-        mode = ["n" "x" "o"];
-      }
-    ];
-  };
+  programs.nixvim.plugins.treesitter.luaConfig.post = ''
+    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    -- -- vim way: ; goes to the direction you were moving.
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+  '';
 }
