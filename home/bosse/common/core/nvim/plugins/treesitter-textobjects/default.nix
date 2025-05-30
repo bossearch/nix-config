916e2253b0eb50e417e341361361671b0a8098d1
bossearch <yudha.bosse@gmail.com>
2025-05-30 22:19:39 +0800

    feat: merge treesitter config and add more plugins
    - added plugins is ts-autotag and ts-comments

--- home/bosse/common/core/nvim/plugins/treesitter-textobjects/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  imports = [./repeat.nix];
-  programs.nixvim.plugins.treesitter-textobjects = {
-    enable = true;
-    move = import ./move.nix;
-  };
-}
--- home/bosse/common/core/nvim/plugins/treesitter/default.nix
+++ home/bosse/common/core/nvim/plugins/treesitter/default.nix
@@ -1,14 +1,92 @@
 {
-  programs.nixvim.plugins.treesitter = {
-    enable = true;
-    lazyLoad = {
+  programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
+      lazyLoad = {
+        settings = {
+          event = ["BufReadPre" "BufNewFile"];
+        };
+      };
       settings = {
-        event = ["BufReadPre" "BufNewFile"];
+        highlight.enable = true;
+        indent.enable = true;
+      };
+      luaConfig.post = ''
+        local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+        -- -- vim way: ; goes to the direction you were moving.
+        vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+        -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+        vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+      '';
+    };
+    treesitter-context = {
+      enable = true;
+      settings = {
+        enable = false;
       };
     };
-    settings = {
-      highlight.enable = true;
-      indent.enable = true;
+    treesitter-textobjects = {
+      enable = true;
+      move = {
+        enable = true;
+        setJumps = true;
+        gotoNextStart.__raw = ''
+          {
+            ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+            ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+            ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+            ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+          }
+        '';
+        gotoNextEnd.__raw = ''
+          {
+            ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+            ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+            ["]C"] = { query = "@class.outer", desc = "Next class end" },
+            ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+          }
+        '';
+        gotoPreviousStart.__raw = ''
+          {
+            ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+            ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+            ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+            ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+            ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+          }
+        '';
+        gotoPreviousEnd.__raw = ''
+          {
+            ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+            ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+            ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+            ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+            ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+          }
+        '';
+      };
+    };
+    ts-autotag = {
+      enable = true;
+      lazyLoad = {
+        settings = {
+          event = ["BufReadPre" "BufNewFile"];
+        };
+      };
+    };
+    ts-comments = {
+      enable = true;
+      lazyLoad = {
+        settings = {
+          event = ["BufReadPre" "BufNewFile"];
+        };
+      };
     };
   };
 }
