2feed26df0c3ad6dc3ac2b143b78006676ac75fc
bossearch <yudha.bosse@gmail.com>
2025-06-01 03:57:35 +0800

    fix: add lsp onAttach to disable same word hl

--- home/bosse/common/core/nvim/plugins/lsp/default.nix
+++ home/bosse/common/core/nvim/plugins/lsp/default.nix
@@ -24,6 +24,10 @@ in {
         # Be aware that you also will need to properly configure your LSP server to
         # provide the inlay hints.
         inlayHints = false;
+        # uncomment this if you're not want lsp hl some word
+        # onAttach = ''
+        #   client.server_capabilities.documentHighlightProvider = false
+        # '';
         luaConfig.post = ''
           local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
           for type, icon in pairs(signs) do
