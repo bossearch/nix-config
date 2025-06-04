5f42c4a82e75cd0228e56ccbc8b6694f280f19fc
bossearch <yudha.bosse@gmail.com>
2025-06-04 23:11:50 +0800

    fix: launcher from using zsh to fish

--- home/bosse/common/optional/waybar/scripts/launcher/launcher-left.nix
+++ home/bosse/common/optional/waybar/scripts/launcher/launcher-left.nix
@@ -4,8 +4,9 @@
     text = ''
       #!/usr/bin/env bash
       hyprctl dispatch movecursor 1280 720
-      kitty -T fastfetch -o cursor=#1a1b26 -o cursor_trail=0  -e zsh \
-        -c "fastfetch --logo-position right --logo-padding-right 0; read -k"
+      kitty -T fastfetch -o cursor=#1a1b26 -o cursor_trail=0 -e fish -c '
+        fastfetch --logo-position right --logo-padding-right 0
+        bash -c "read -n 1"'
     '';
   };
 }
