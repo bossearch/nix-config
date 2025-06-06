8031c2a46b368e08a695512885d626547686d31c
bossearch <yudha.bosse@gmail.com>
2025-06-06 23:21:41 +0800

    fix: pin vaults to v0.7.1 newer version not work

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
   # Packages with an actual source
   sysboard = pkgs.callPackage ./sysboard {};
   varia = pkgs.callPackage ./varia {};
+  vaults = pkgs.callPackage ./vaults {};
   zsh-auto-notify = pkgs.callPackage ./zsh-auto-notify {};
 }
