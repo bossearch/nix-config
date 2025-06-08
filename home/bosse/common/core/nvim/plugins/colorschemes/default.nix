91bc28af3b87c40c4141c98dcd3a6d3dc90ed143
bossearch <yudha.bosse@gmail.com>
2025-06-08 17:11:41 +0800

    fix: nixvim rename tokyonight dir to colorschemes

---
+++ home/bosse/common/core/nvim/plugins/colorschemes/default.nix
@@ -0,0 +1,13 @@
+{
+  programs.nixvim.colorschemes = {
+    tokyonight = {
+      enable = true;
+      settings = {
+        style = "night";
+      };
+      lazyLoad = {
+        enable = true;
+      };
+    };
+  };
+}
