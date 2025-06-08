9988fae69724dd718329c27cefa9ea85c9e831b2
bossearch <yudha.bosse@gmail.com>
2025-06-08 09:44:07 +0800

    fix: buku symlinks to use absolute path

--- home/bosse/common/core/cli/buku/default.nix
+++ home/bosse/common/core/cli/buku/default.nix
@@ -6,7 +6,7 @@
   home.packages = [pkgs.buku];
 
   home.file.".local/share/buku" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./data;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-config/home/bosse/common/core/cli/buku/data";
     recursive = true;
   };
 }
