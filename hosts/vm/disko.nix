2676d0e529aa7b054eba45340f88098ae194124a
bossearch <yudha.bosse@gmail.com>
2025-06-19 20:29:44 +0800

    chore: add disko config for main host

--- hosts/vm/disko.nix
+++ hosts/vm/disko.nix
@@ -1,27 +1,54 @@
 {inputs, ...}: {
   imports = [inputs.disko.nixosModules.disko];
-  disko.devices.disk.vda = {
-    type = "disk";
-    device = "/dev/vda";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          size = "1G";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = ["umask=0077"];
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
           };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+
+          luks = {
+            size = "100%";
+            label = "luks";
+            content = {
+              type = "luks";
+              name = "enc";
+              passwordFile = "/key/luks";
+              settings.keyFile = "/key/luks.key";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-L" "nixos" "-f"];
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = ["noatime" "compress=zstd" "subvol=@root"];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = ["noatime" "compress=zstd" "subvol=@home"];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["noatime" "compress=zstd" "subvol=@nix"];
+                  };
+                  "@swap" = {
+                    mountpoint = "/swap";
+                    mountOptions = ["noatime" "subvol=@swap"];
+                    swap.swapfile.size = "1G";
+                  };
+                };
+              };
+            };
           };
         };
       };
--- hosts/vm/hardware-configuration-vm.nix
+++ hosts/vm/hardware-configuration-vm.nix
@@ -4,35 +4,16 @@
 {
   lib,
   modulesPath,
-  pkgs,
   ...
 }: {
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.kernelModules = ["uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1"];
   boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
-  boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-    mkdir -m 0755 -p /key
-    sleep 2 # Give the USB device time to initialize
-    for i in $(seq 1 10); do
-      if [ -e /dev/disk/by-uuid/DA30-0796 ]; then
-        echo ">> Found USB device."
-        mount -n -t vfat -o ro /dev/disk/by-uuid/DA30-0796 /key && break
-      fi
-      sleep 1
-    done
-  '';
-  boot.initrd.luks.devices.enc = {
-    keyFile = "/key/luks.key";
-    allowDiscards = true;
-    keyFileSize = 4096;
-    fallbackToPassword = true; # Allow password if USB key is missing
-    preLVM = false;
-  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
