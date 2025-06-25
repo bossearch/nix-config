3e514082656500316429ff025a1e02b6b190e8eb
bossearch <yudha.bosse@gmail.com>
2025-06-26 00:07:52 +0800

    feat: add impermanence config for vm

--- hosts/common/core/ssh.nix
+++ hosts/common/core/ssh.nix
@@ -1,15 +1,31 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  # Sops needs acess to the keys before the persist dirs are even mounted; so
+  # just persisting the keys won't work, we must point at /persist
+  hasOptinPersistence = lib.hasAttrByPath ["environment" "persistence" "/persist"] config;
+in {
   services.openssh = {
     enable = lib.mkForce true;
     settings = {
       PasswordAuthentication = true;
       PermitRootLogin = "no";
     };
-    hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
+    hostKeys =
+      if hasOptinPersistence
+      then [
+        {
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ]
+      else [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
   };
 }
