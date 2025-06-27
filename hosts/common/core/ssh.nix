78588da91ac31739a3e6d334a4ec7a165e180f5f
bossearch <yudha.bosse@gmail.com>
2025-06-27 17:18:16 +0800

    fix: avoid infinite recursion for impermanence

--- hosts/common/core/ssh.nix
+++ hosts/common/core/ssh.nix
@@ -3,9 +3,10 @@
   lib,
   ...
 }: let
-  # Sops needs acess to the keys before the persist dirs are even mounted; so
-  # just persisting the keys won't work, we must point at /persist
-  hasOptinPersistence = lib.hasAttrByPath ["environment" "persistence" "/persist"] config;
+  keyDir =
+    if config.spec.impermanence
+    then "/persist/etc/ssh"
+    else "/etc/ssh";
 in {
   services.openssh = {
     enable = lib.mkForce true;
@@ -13,19 +14,11 @@ in {
       PasswordAuthentication = true;
       PermitRootLogin = "no";
     };
-    hostKeys =
-      if hasOptinPersistence
-      then [
-        {
-          path = "/persist/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ]
-      else [
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
+    hostKeys = [
+      {
+        path = "${keyDir}/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 }
