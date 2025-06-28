167dec0570da7a426387d7407a14b84da0f2a9da
bossearch <yudha.bosse@gmail.com>
2025-06-29 06:39:15 +0800

    feat: add nix colors to nix-on-droid config

--- home/droid/default.nix
+++ home/droid/default.nix
@@ -1,54 +1,15 @@
-# {lib, ...}: let
-#   inherit (lib) mkOption types;
-# in {
-#   imports = [
-#     ../common/core/colors.nix
-#     ../common/core/shell
-#     ../common/core/cli/fastfetch
-#     ../common/core/cli/fzf
-#     ../common/core/cli/lazygit
-#     ../common/core/cli/tealdeer
-#     # ../common/core/cli/yazi
-#   ];
-#
-#   programs.bat = {
-#     enable = true;
-#     config = {
-#       pager = "less -FR";
-#       theme = "ansi";
-#     };
-#   };
-#   programs.zoxide = {
-#     enable = true;
-#     options = [
-#       "--cmd cd"
-#     ];
-#   };
-#
-#   options.spec = mkOption {
-#     type = types.submodule {
-#       options = {
-#         theme = mkOption {
-#           default = lib.mkDefault "tokyo-night-dark";
-#           type = lib.types.enum [
-#             "catppuccin-mocha"
-#             "tokyo-night-dark"
-#           ];
-#         };
-#       };
-#     };
-#   };
-#
-#   config.spec.theme = "tokyo-night-dark";
-#
-#   # Do not change these future me !
-#   home.stateVersion = "24.05"; # Please read the comment before changing.
-# }
-{lib, ...}: let
-  inherit (lib) mkOption types;
+{inputs, ...}: let
+  nix-colors = inputs.nix-colors;
+  theme = "tokyo-night-dark";
 in {
+  colorScheme =
+    nix-colors.lib.schemeFromYAML
+    theme
+    (builtins.readFile ../../modules/themes/${theme}.yaml);
+
   imports = [
-    ../common/core/colors.nix
+    nix-colors.homeManagerModules.default
+    # ../common/core/colors.nix
     ../common/core/shell
     ../common/core/cli/fastfetch
     ../common/core/cli/fzf
@@ -57,37 +18,19 @@ in {
     # ../common/core/cli/yazi
   ];
 
-  options.spec = mkOption {
-    type = types.submodule {
-      options = {
-        theme = mkOption {
-          default = lib.mkDefault "tokyo-night-dark";
-          type = types.enum [
-            "catppuccin-mocha"
-            "tokyo-night-dark"
-          ];
-        };
-      };
+  programs.bat = {
+    enable = true;
+    config = {
+      pager = "less -FR";
+      theme = "ansi";
     };
   };
 
-  config = {
-    spec.theme = "tokyo-night-dark";
-
-    programs.bat = {
-      enable = true;
-      config = {
-        pager = "less -FR";
-        theme = "ansi";
-      };
-    };
-
-    programs.zoxide = {
-      enable = true;
-      options = ["--cmd cd"];
-    };
-
-    # Do not change these future me !
-    home.stateVersion = "24.05";
+  programs.zoxide = {
+    enable = true;
+    options = ["--cmd cd"];
   };
+
+  # Do not change these future me !
+  home.stateVersion = "24.05";
 }
