48f31c76748caff41d9273f0ec8d00159d8dd2ce
bossearch <yudha.bosse@gmail.com>
2025-06-30 03:35:56 +0800

    fix: buku source pathing to use new path

--- home/common/core/cli/buku/default.nix
+++ home/common/core/cli/buku/default.nix
@@ -6,7 +6,7 @@
   home.packages = [pkgs.buku];
 
   home.file.".local/share/buku" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-config/home/bosse/common/core/cli/buku/data";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-config/home/common/core/cli/buku/data";
     recursive = true;
   };
 }
