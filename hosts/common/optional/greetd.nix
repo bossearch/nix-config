fb6ea6dc81c1401e53a2f97390a4cb929d5732ed
bossearch <yudha.bosse@gmail.com>
2025-07-05 08:14:21 +0800

    feat: greetd fallback to tty2 if autologin fail

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -2,15 +2,28 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  fallbackScript = pkgs.writeShellScript "start-desktop" ''
+    # Make sure shell itself doesn't print anything either
+    exec 1>/dev/null 2>&1
+
+    if command -v Hyprland >/dev/null 2>&1; then
+      exec Hyprland
+    else
+      chvt 2
+    fi
+  '';
+in {
   services.greetd = {
     enable = true;
+    vt = 1;
     settings = rec {
       initial_session = {
-        command = "${pkgs.bash}/bin/bash -c 'Hyprland >/dev/null'";
-        user = "${config.spec.userName}";
+        command = fallbackScript;
+        user = config.spec.userName;
       };
       default_session = initial_session;
     };
   };
+  systemd.services."getty@tty2".enable = true;
 }
