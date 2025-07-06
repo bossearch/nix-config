e14537f32d97ec4a52af2bb9b5d92dbfc3889347
bossearch <yudha.bosse@gmail.com>
2025-07-06 11:53:59 +0800

    feat: change plymouth theme from adi1090x

--- hosts/common/optional/silent-boot.nix
+++ hosts/common/optional/silent-boot.nix
@@ -1,11 +1,22 @@
-{
+{pkgs, ...}: let
+  themes = pkgs.adi1090x-plymouth-themes.override {
+    selected_themes = ["spinner_alt"];
+  };
+in {
   boot = {
     #silent boot
-    kernelParams = ["quiet" "udev.log_level=0" "systemd.show_status=false"];
+    kernelParams = [
+      "quiet"
+      "udev.log_level=0"
+      "systemd.show_status=false"
+    ];
     initrd.verbose = false;
     consoleLogLevel = 0;
     #plymouth
-    plymouth.enable = true;
-    plymouth.theme = "breeze";
+    plymouth = {
+      enable = true;
+      theme = "spinner_alt";
+      themePackages = [themes];
+    };
   };
 }
