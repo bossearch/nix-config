9405ce8e135ae564017cc79aa5e53db2bbd3eeb0
bossearch <yudha.bosse@gmail.com>
2025-09-19 18:09:56 +0800

    fix: unpin vaults v0.7.1 since newest ver works

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,7 @@
   # Packages with an actual source
   sysboard = pkgs.callPackage ./sysboard {};
   varia = pkgs.callPackage ./varia {};
-  vaults = pkgs.callPackage ./vaults {};
+  # vaults = pkgs.callPackage ./vaults {};
   xdm = pkgs.callPackage ./xdm {};
   zsh-auto-notify = pkgs.callPackage ./zsh-auto-notify {};
 }
