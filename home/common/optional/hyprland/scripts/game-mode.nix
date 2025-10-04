81652a0f7908d04cea35aca99012aa199a475003
bossearch <yudha.bosse@gmail.com>
2025-10-04 17:55:34 +0800

    fix: update gamemode scripts to works better

--- home/common/optional/hyprland/scripts/game-mode.nix
+++ home/common/optional/hyprland/scripts/game-mode.nix
@@ -5,38 +5,41 @@
       #!/usr/bin/env bash
 
       current_workspace=$(hyprctl activewindow | grep "workspace: " | sed -E 's/.*workspace: .*\((.*)\)/\1/')
+      gamemode_file=~/.cache/${config.spec.userName}/gamemode
 
-      if [ -f ~/.cache/${config.spec.userName}/gamemode ]; then
+      apply_gamemode() {
         hyprctl --batch "\
-          dispatch togglespecialworkspace gamespace;\
-          reload"
-        rm ~/.cache/${config.spec.userName}/gamemode
-        notify-send -a game_off "Game Mode" "Deactivated"
-      else
-        if [ "$current_workspace" = "special:gamespace" ]; then
-          hyprctl --batch "\
-            keyword unbind CONTROL, Q;\
-            keyword unbind CONTROL, SPACE;\
-            keyword unbind CONTROL, mouse:273;\
-            keyword decoration:blur:enabled 0;\
-            keyword decoration:active_opacity 1;\
-            keyword decoration:inactive_opacity 1;\
-            keyword decoration:rounding 0"
-          touch ~/.cache/${config.spec.userName}/gamemode
-          notify-send -a game_on "Game Mode" "Activated"
-        else
-          hyprctl --batch "\
-            dispatch togglespecialworkspace gamespace;\
-            keyword unbind CONTROL, Q;\
-            keyword unbind CONTROL, SPACE;\
-            keyword unbind CONTROL, mouse:273;\
-            keyword decoration:blur:enabled 0;\
-            keyword decoration:active_opacity 1;\
-            keyword decoration:inactive_opacity 1;\
-            keyword decoration:rounding 0"
-          touch ~/.cache/${config.spec.userName}/gamemode
-          notify-send -a game_on "Game Mode" "Activated"
+          keyword unbind CONTROL, Q;\
+          keyword unbind CONTROL, SPACE;\
+          keyword unbind CONTROL, mouse:273;\
+          keyword decoration:blur:enabled 0;\
+          keyword decoration:active_opacity 1;\
+          keyword decoration:inactive_opacity 1;\
+          keyword decoration:rounding 0"
+      }
+
+      activate_gamemode() {
+        if [ "$current_workspace" != "special:gamespace" ]; then
+          hyprctl dispatch togglespecialworkspace gamespace
         fi
+        apply_gamemode
+        touch "$gamemode_file"
+        notify-send -a game_on "Game Mode" "Activated"
+      }
+
+      deactivate_gamemode() {
+        if [ "$current_workspace" = "special:gamespace" ]; then
+          hyprctl dispatch togglespecialworkspace gamespace
+        fi
+        hyprctl reload
+        rm -f "$gamemode_file"
+        notify-send -a game_off "Game Mode" "Deactivated"
+      }
+
+      if [ -f "$gamemode_file" ]; then
+        deactivate_gamemode
+      else
+        activate_gamemode
       fi
     '';
   };
