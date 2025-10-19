b82131eea4bde8baec126ffa82a581343ad285a3
bossearch <yudha.bosse@gmail.com>
2025-10-19 16:44:41 +0800

    fix: specified ovmf package for virt-manager

--- hosts/common/optional/virt-manager.nix
+++ hosts/common/optional/virt-manager.nix
@@ -5,7 +5,10 @@
     qemu = {
       package = pkgs.qemu_kvm;
       swtpm.enable = true;
-      ovmf.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [pkgs.OVMFFull];
+      };
       vhostUserPackages = with pkgs; [virtiofsd];
     };
   };
