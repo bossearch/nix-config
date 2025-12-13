266ca8119a72e820cbac20d932d6e0d0029ccef0
bossearch <yudha.bosse@gmail.com>
2025-12-13 19:49:54 +0800

    fix: fzf drun scirpt to work with steam

--- home/common/core/cli/fzf/extra/drun.nix
+++ home/common/core/cli/fzf/extra/drun.nix
@@ -1,25 +1,27 @@
-{
+{config, ...}: {
   home.file.".config/fzf/extra/drun.sh" = {
     executable = true;
     text = ''
       #!/usr/bin/env bash
 
       # Read the app names and Exec commands from the saved txt file
-      apps_list=$(cat "$HOME/.cache/$(whoami)/drun.txt")
+      APPS_LIST=$(cat "$HOME/.cache/${config.spec.userName}/drun.txt")
 
       # Use fzf to list app names and select multiple
-      selected=$(
-        echo "$apps_list" | cut -d'|' -f1 | sort |
+      SELECTED=$(
+        echo "$APPS_LIST" | cut -d'|' -f1 | sort |
           fzf -m --layout=default --border=none --bind 'tab:select+backward-kill-word'
       )
 
       # Run each selected application's Exec command
-      while IFS= read -r app; do
-        if [ -n "$app" ]; then
-          exec_command=$(echo "$apps_list" | grep "^$app|" | cut -d'|' -f2)
-          setsid bash -c "$exec_command" >/dev/null 2>&1 & disown
+      while IFS= read -r APP; do
+        if [ -n "$APP" ]; then
+          EXEC_COMMAND=$(echo "$APPS_LIST" | grep "^$APP|" | cut -d'|' -f2)
+          # read -r -a cmd_array <<<"$EXEC_COMMAND"
+          # setsid --fork "''${cmd_array[@]}" >/dev/null 2>&1 </dev/null
+          setsid --fork bash -c "$EXEC_COMMAND" >/dev/null 2>&1 </dev/null
         fi
-      done <<< "$selected"
+      done <<<"$SELECTED"
 
       sleep 0.1
     '';
