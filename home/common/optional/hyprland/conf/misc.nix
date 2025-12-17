597db165bf549968450c7c85b48e74c06d76bd19
bossearch <yudha.bosse@gmail.com>
2025-12-17 23:28:01 +0800

    feat: disable hyprland update and donation messages

--- home/common/optional/hyprland/conf/misc.nix
+++ home/common/optional/hyprland/conf/misc.nix
@@ -4,6 +4,11 @@
     ### MISC ###
     ############
 
+    ecosystem {
+        no_update_news = true
+        no_donation_nag = true
+    }
+
     misc {
     #    focus_on_activate = true
         allow_session_lock_restore = true
