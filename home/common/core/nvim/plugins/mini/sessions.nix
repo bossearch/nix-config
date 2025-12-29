8ca450d559c614db197d35498ff780091f9d639b
bossearch <yudha.bosse@gmail.com>
2025-12-29 22:02:33 +0800

    fix: nixvim make mini sessions not saving fyler

--- home/common/core/nvim/plugins/mini/sessions.nix
+++ home/common/core/nvim/plugins/mini/sessions.nix
@@ -1,6 +1,6 @@
 {
   autoread = false; # Auto-load session if no file is given
-  autowrite = true; # Auto-save session before exiting
+  autowrite = false; # Auto-save session before exiting
 
   directory.__raw = ''vim.fn.stdpath ("state") .. "/session"''; # ~/.local/state/nvim/session
   file = ""; # Save session as 'Session.vim' inside the directory
@@ -14,7 +14,15 @@
   hooks = {
     pre = {
       read = null;
-      write = null;
+      write.__raw = ''
+        function ()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_get_name(buf):match("^fyler://") then
+              vim.api.nvim_buf_delete(buf, { force = true })
+            end
+          end
+        end
+      '';
       delete = null;
     };
     post = {
@@ -23,7 +31,13 @@
           vim.notify("Session restored!")
         end
       '';
-      write = null;
+      write.__raw = ''
+        function()
+          local file_win = vim.api.nvim_get_current_win()
+          require("fyler").open({ dir = vim.fn.getcwd(), kind="split_left" })
+          require("lib.util").fyler_width(file_win)
+        end
+      '';
       delete = null;
     };
   };
