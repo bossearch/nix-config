b54e47e649984ebe3145b1c274c982165613451e
bossearch <yudha.bosse@gmail.com>
2026-01-15 00:40:54 +0800

    fix: fish change furl functions from nohup to setsid

--- home/common/core/shell/functions/fish/furl.nix
+++ home/common/core/shell/functions/fish/furl.nix
@@ -7,7 +7,7 @@
             echo "$content" | grep -oE '(http?s://)?www\.[a-zA-Z](-?[a-zA-Z0-9])+\.[a-zA-Z]{2,}(/\S+)?' | grep -vE '^https?://' | sed 's|^|http://|'
             echo "$content" | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}(:[0-9]{1,5})?(/\S+)?' | sed 's|^|http://|'
             echo "$content" | grep -oE '(ssh://)?git@\S+' | sed -E 's/:/\//g; s/^(ssh\/\/\/)?git@(.+)$/https:\/\/\2/'
-            echo "$content" | grep -oE "['\"]([_A-Za-z0-9-]+/[_.A-Za-z0-9-]+)['\"]" | sed "s/['\"]//g" | sed 's|^|https://github.com/|'
+            echo "$content" | grep -oE '(~/|/home/'$USER'/)[A-Za-z0-9._\/-]+' | sed "s|^~|$HOME|"
         end | sort -u | grep -v '^$'
     )
     if test (count $all_urls) -eq 0
@@ -15,13 +15,14 @@
         return 1
     end
     set -l selected (printf '%s\n' $all_urls | fzf --prompt="Open URL > ")
+
     if test -n "$selected"
         if type -q xdg-open
-            nohup xdg-open $selected >/dev/null 2>&1 &
+            setsid --fork xdg-open "$selected" >/dev/null 2>&1 </dev/null
         else if type -q open
-            nohup open $selected >/dev/null 2>&1 &
+            setsid --fork open "$selected" >/dev/null 2>&1 </dev/null
         else if set -q BROWSER
-            nohup $BROWSER $selected >/dev/null 2>&1 &
+            setsid --fork $BROWSER "$selected" >/dev/null 2>&1 </dev/null
         else
             echo "No suitable method to open URL found."
             return 1
