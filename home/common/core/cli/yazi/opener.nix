13b177b84c1525e79f60fc523a175b1e0bdfc9fa
bossearch <yudha.bosse@gmail.com>
2026-01-15 00:45:15 +0800

    fix: yazi add space prefixes before launch nvim opener

--- home/common/core/cli/yazi/opener.nix
+++ home/common/core/cli/yazi/opener.nix
@@ -11,7 +11,7 @@
         ya emit quit
         SOCKET=''${SOCKET:-''${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/tmux-$(id -u)/default}
         CURRENT_PANE=$(tmux -S "$SOCKET" display-message -p '#{session_name}:#{window_index}.#{pane_index}')
-        tmux -S "$SOCKET" send-keys -t "$CURRENT_PANE" "nvim \"$FILE\"" C-m
+        tmux -S "$SOCKET" send-keys -t "$CURRENT_PANE" " nvim \"$FILE\"" C-m
       fi
     '';
   };
