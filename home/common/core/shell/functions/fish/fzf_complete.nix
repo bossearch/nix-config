fe0cad81a03ff24b78c08f2215aad4d295ca050a
bossearch <yudha.bosse@gmail.com>
2026-02-04 22:34:31 +0800

    fix: fish fzf_complete clean empty query - remove multi select handler - remove __fzf_complete_apply function just use repetitive command

--- home/common/core/shell/functions/fish/fzf_complete.nix
+++ home/common/core/shell/functions/fish/fzf_complete.nix
@@ -42,7 +42,9 @@
         # 2. Handle single result
         if test (count $complist) -eq 1
             set -l result (string replace -r '\t.*$' ''\'' -- $complist[1])
-            __fzf_complete_apply "$result"
+            # apply __fzf_complete
+            commandline -t -- $result
+            commandline -f repaint
             return
         end
 
@@ -53,30 +55,24 @@
         | eval (__fzfcmd) (string escape --no-quoted -- $fzf_query) --print-query (__fzf_complete_opts) \
         | while read -l r
             if test -z "$query"
-                set query $r
-            else
                 set -l cleaned (string replace -r '\t.*$' ''\'' -- $r)
-                set result $result $cleaned
+                set query $cleaned
+             else
+                set -l cleaned (string replace -r '\t.*$' ''\'' -- $r)
+                set result $cleaned
             end
         end
 
+        # cancel handle
         if test -z "$query" -a -z "$result"
             commandline -f repaint
             return
         end
 
+        # if query not in result print query
         test -z "$result"; and set result $query
 
-        for i in (seq (count $result))
-            __fzf_complete_apply $result[$i]
-            if test $i -lt (count $result)
-                commandline -i ' '
-            end
-        end
-    end
-
-    function __fzf_complete_apply -a result
-        commandline -t ""
+        # apply __fzf_complete
         commandline -t -- $result
         commandline -f repaint
     end
