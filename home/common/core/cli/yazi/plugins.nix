fcf4faa67749481d4c9ef61d40d8e856b34450a8
bossearch <yudha.bosse@gmail.com>
2026-02-11 00:56:32 +0800

    refactor: yazi change mediainfo to pkgs.yaziPlugins
    - fix ouch plugins typo

--- home/common/core/cli/yazi/plugins.nix
+++ home/common/core/cli/yazi/plugins.nix
@@ -1,19 +1,12 @@
-{pkgs, ...}: let
-  mediainfo-plugins = pkgs.fetchFromGitHub {
-    owner = "boydaihungst";
-    repo = "mediainfo.yazi";
-    rev = "af8bdf47a1f4dcfefe433aa2134b04eb9c75a10b";
-    sha256 = "sha256-io3HyoUniWZu+0efZfbhXn8JoG5p2/lFeH/FguVvjSY=";
-  };
-in {
+{pkgs, ...}: {
   programs.yazi.plugins = {
     chmod = pkgs.yaziPlugins.chmod;
     full-border = pkgs.yaziPlugins.full-border;
     git = pkgs.yaziPlugins.git;
     no-status = pkgs.yaziPlugins.no-status;
-    ouch = pkgs.yaziPlugins.smart-enter;
+    ouch = pkgs.yaziPlugins.ouch;
     smart-enter = pkgs.yaziPlugins.smart-enter;
     smart-paste = pkgs.yaziPlugins.smart-paste;
-    mediainfo = "${mediainfo-plugins}";
+    mediainfo = pkgs.yaziPlugins.mediainfo;
   };
 }
