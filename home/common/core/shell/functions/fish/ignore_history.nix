938ae878b5d17d4287c049c46c2e72aad9f17083
bossearch <yudha.bosse@gmail.com>
2026-02-17 22:54:10 +0800

    fix: add seniorpw and other commands to ignore_history

--- home/common/core/shell/functions/fish/ignore_history.nix
+++ home/common/core/shell/functions/fish/ignore_history.nix
@@ -2,7 +2,11 @@
   ignore_history = {
     onEvent = "fish_postexec";
     body = ''
-      set -l ignore_list pw senior TmuxNavigateDown TmuxNavigateLeft TmuxNavigateRight TmuxNavigateUp
+      set -l ignore_list \
+          pw senior \
+          yy sy c fish \
+          TmuxNavigateDown TmuxNavigateLeft TmuxNavigateRight TmuxNavigateUp
+
       set -l cleaned_cmd (string trim -- $argv[1])
       if contains -- $cleaned_cmd $ignore_list
           or string match -q " *" $argv[1]
