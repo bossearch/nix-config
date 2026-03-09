de3b4a92fce31bf0701ddd193745f633a1c4a898
bossearch <yudha.bosse@gmail.com>
2026-03-10 00:33:13 +0800

    fix: nvim move plugin autocmds in lib.optionals

--- options/home-manager/core/nvim/core/autocmd.nix
+++ options/home-manager/core/nvim/core/autocmd.nix
@@ -1,203 +1,211 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.nixvim = {
-    autoCmd = [
-      # Highlight on yank
-      {
-        event = ["TextYankPost"];
-        pattern = ["*"];
-        callback.__raw = ''
-          function()
-            vim.hl.on_yank()
-          end
-        '';
-        desc = "Highlight selection on yank";
-      }
-
-      # Resize splits if window is resized
-      {
-        event = ["VimResized"];
-        callback.__raw = ''
-          function()
-            vim.cmd("tabdo wincmd =")
-          end
-        '';
-        desc = "Resize splits on window resize";
-      }
-
-      # Close some filetypes with <q>
-      {
-        event = ["FileType"];
-        pattern = [
-          "git"
-          "help"
-          "lspinfo"
-          "man"
-          "notify"
-          "qf"
-          "health"
-          "oil"
-        ];
-        callback.__raw = ''
-          function(event)
-            vim.bo[event.buf].buflisted = false
-            vim.keymap.set("n", "q", "<cmd>q!<cr>", { buffer = event.buf, silent = true })
-          end
-        '';
-        desc = "Close certain filetypes with <q>";
-      }
-
-      # Auto-create directory on save if necessary
-      {
-        event = ["BufWritePre"];
-        callback.__raw = ''
-          function(event)
-            local file = vim.uv.fs_realpath(event.match) or event.match
-            if vim.bo.filetype == "oil" then
-              return
+    autoCmd =
+      [
+        # Highlight on yank
+        {
+          event = ["TextYankPost"];
+          pattern = ["*"];
+          callback.__raw = ''
+            function()
+              vim.hl.on_yank()
             end
-            vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-          end
-        '';
-        desc = "Auto-create directory before saving file";
-      }
+          '';
+          desc = "Highlight selection on yank";
+        }
 
-      # Remove trailing whitespace on save
-      {
-        event = ["BufWritePre"];
-        pattern = ["*"];
-        callback.__raw = ''
-          function()
-            local save_cursor = vim.fn.getpos(".")
-            pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-            vim.fn.setpos(".", save_cursor)
-          end
-        '';
-        desc = "Remove trailing whitespace on save";
-      }
-
-      # Set colorcolumn for git commit messages
-      {
-        event = ["FileType"];
-        pattern = ["gitcommit"];
-        callback.__raw = ''
-          function()
-            vim.opt_local.colorcolumn = { "50", "72" }
-          end
-        '';
-        desc = "Set colorcolumn for git commit messages";
-      }
-
-      # Set custom hl for mini.base16
-      {
-        event = ["VimEnter"];
-        callback.__raw = ''
-          function()
-            require("lib.util").set_highlights()
-          end
-        '';
-      }
-
-      # auto open fyler
-      {
-        event = ["BufWinEnter"];
-        callback.__raw = ''
-          function(args)
-            local buf = args.buf or 0
-            if vim.bo[buf].buftype ~= "" then
-              return
+        # Resize splits if window is resized
+        {
+          event = ["VimResized"];
+          callback.__raw = ''
+            function()
+              vim.cmd("tabdo wincmd =")
             end
+          '';
+          desc = "Resize splits on window resize";
+        }
 
-            local filename = vim.api.nvim_buf_get_name(buf)
-            if filename == "" then
-              return
+        # Close some filetypes with <q>
+        {
+          event = ["FileType"];
+          pattern = [
+            "git"
+            "help"
+            "lspinfo"
+            "man"
+            "notify"
+            "qf"
+            "health"
+            "oil"
+          ];
+          callback.__raw = ''
+            function(event)
+              vim.bo[event.buf].buflisted = false
+              vim.keymap.set("n", "q", "<cmd>q!<cr>", { buffer = event.buf, silent = true })
             end
+          '';
+          desc = "Close certain filetypes with <q>";
+        }
 
-            local basename = vim.fn.fnamemodify(filename, ":t")
-            local exclude_names = { ".hm.log", ".rb.log" }
-            for _, name in ipairs(exclude_names) do
-              if basename == name then return end
-            end
-
-            local ft = vim.bo[buf].filetype
-            local ignore_filetypes = {
-              "help", "gitcommit", "gitrebase", "gitconfig", "notify",
-              "qf", "terminal", "toggleterm", "checkhealth", "lspinfo",
-              "snacks_dashboard", "Avante.*", "dapui_.*"
-            }
-            for _, pattern in ipairs(ignore_filetypes) do
-              if ft:match(pattern) then
+        # Auto-create directory on save if necessary
+        {
+          event = ["BufWritePre"];
+          callback.__raw = ''
+            function(event)
+              local file = vim.uv.fs_realpath(event.match) or event.match
+              if vim.bo.filetype == "oil" then
                 return
               end
+              vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
             end
+          '';
+          desc = "Auto-create directory before saving file";
+        }
 
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              local win_buf = vim.api.nvim_win_get_buf(win)
-              if vim.bo[win_buf].filetype == "Fyler" then
+        # Remove trailing whitespace on save
+        {
+          event = ["BufWritePre"];
+          pattern = ["*"];
+          callback.__raw = ''
+            function()
+              local save_cursor = vim.fn.getpos(".")
+              pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+              vim.fn.setpos(".", save_cursor)
+            end
+          '';
+          desc = "Remove trailing whitespace on save";
+        }
+
+        # Set colorcolumn for git commit messages
+        {
+          event = ["FileType"];
+          pattern = ["gitcommit"];
+          callback.__raw = ''
+            function()
+              vim.opt_local.colorcolumn = { "50", "72" }
+            end
+          '';
+          desc = "Set colorcolumn for git commit messages";
+        }
+
+        # Set custom hl for mini.base16
+        {
+          event = ["VimEnter"];
+          callback.__raw = ''
+            function()
+              require("lib.util").set_highlights()
+            end
+          '';
+        }
+
+        # make qf split below others
+        {
+          event = ["FileType"];
+          pattern = ["qf"];
+          callback.__raw = ''
+            function()
+              vim.cmd("wincmd J")
+            end
+          '';
+        }
+      ]
+      ++ lib.optionals (builtins.elem pkgs.vimPlugins.fyler-nvim config.programs.nixvim.extraPlugins) [
+        # auto open fyler
+        {
+          event = ["BufWinEnter"];
+          callback.__raw = ''
+            function(args)
+              local buf = args.buf or 0
+              if vim.bo[buf].buftype ~= "" then
                 return
               end
+
+              local filename = vim.api.nvim_buf_get_name(buf)
+              if filename == "" then
+                return
+              end
+
+              local basename = vim.fn.fnamemodify(filename, ":t")
+              local exclude_names = { ".hm.log", ".rb.log" }
+              for _, name in ipairs(exclude_names) do
+                if basename == name then return end
+              end
+
+              local ft = vim.bo[buf].filetype
+              local ignore_filetypes = {
+                "help", "gitcommit", "gitrebase", "gitconfig", "notify",
+                "qf", "terminal", "toggleterm", "checkhealth", "lspinfo",
+                "snacks_dashboard", "Avante.*", "dapui_.*"
+              }
+              for _, pattern in ipairs(ignore_filetypes) do
+                if ft:match(pattern) then
+                  return
+                end
+              end
+
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local win_buf = vim.api.nvim_win_get_buf(win)
+                if vim.bo[win_buf].filetype == "Fyler" then
+                  return
+                end
+              end
+
+              local file_win = vim.api.nvim_get_current_win()
+              require("fyler").open({ dir = vim.fn.getcwd(), kind = "split_left" })
+
+              if package.loaded["lib.util"] then
+                require("lib.util").fyler_width(file_win)
+              end
+
+              vim.api.nvim_del_autocmd(args.id)
             end
+          '';
+        }
 
-            local file_win = vim.api.nvim_get_current_win()
-            require("fyler").open({ dir = vim.fn.getcwd(), kind = "split_left" })
-
-            if package.loaded["lib.util"] then
+        # auto refresh fyler if dir change
+        {
+          event = ["DirChanged"];
+          callback.__raw = ''
+            function()
+              local file_win = vim.api.nvim_get_current_win()
+              require("fyler").open({ dir = vim.fn.getcwd(), kind = "split_left" })
               require("lib.util").fyler_width(file_win)
             end
+          '';
+        }
 
-            vim.api.nvim_del_autocmd(args.id)
-          end
-        '';
-      }
-
-      # auto refresh fyler if dir change
-      {
-        event = ["DirChanged"];
-        callback.__raw = ''
-          function()
-            local file_win = vim.api.nvim_get_current_win()
-            require("fyler").open({ dir = vim.fn.getcwd(), kind = "split_left" })
-            require("lib.util").fyler_width(file_win)
-          end
-        '';
-      }
-
-      # close fyler when quit nvim
-      {
-        event = ["QuitPre"];
-        callback.__raw = ''
-          function()
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              local win_buf = vim.api.nvim_win_get_buf(win)
-              if vim.bo[win_buf].filetype == "Fyler" then
-                vim.api.nvim_win_close(win, true)
+        # close fyler when quit nvim
+        {
+          event = ["QuitPre"];
+          callback.__raw = ''
+            function()
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local win_buf = vim.api.nvim_win_get_buf(win)
+                if vim.bo[win_buf].filetype == "Fyler" then
+                  vim.api.nvim_win_close(win, true)
+                end
               end
             end
-          end
-        '';
-      }
-
-      # make qf split below others
-      {
-        event = ["FileType"];
-        pattern = ["qf"];
-        callback.__raw = ''
-          function()
-            vim.cmd("wincmd J")
-          end
-        '';
-      }
-
-      # hide undotree column
-      {
-        event = ["FileType"];
-        pattern = ["undotree"];
-        callback.__raw = ''
-          function()
-            vim.opt_local.signcolumn = "no"
-            vim.opt_local.numberwidth = 1
-          end
-        '';
-      }
-    ];
+          '';
+        }
+      ]
+      ++ lib.optionals (config.programs.nixvim.plugins.undotree.enable) [
+        # hide undotree column
+        {
+          event = ["FileType"];
+          pattern = ["undotree"];
+          callback.__raw = ''
+            function()
+              vim.opt_local.signcolumn = "no"
+              vim.opt_local.numberwidth = 1
+            end
+          '';
+        }
+      ];
   };
 }
