0d5f04db0fe54d68265fcb79b905b7e520c1a40d
bossearch <yudha.bosse@gmail.com>
2026-03-12 22:58:07 +0800

    fix: impermanence use native persistence for /home

--- options/nixos/core/impermanence.nix
+++ options/nixos/core/impermanence.nix
@@ -18,6 +18,13 @@ in {
         hideMounts = true;
         directories =
           [
+            # persist home directory
+            {
+              directory = "/home/${hosts.username}";
+              user = "${hosts.username}";
+              group = "users";
+              mode = "0700";
+            }
             {
               directory = "/etc/NetworkManager/system-connections";
               user = "root";
@@ -84,20 +91,7 @@ in {
         files = [
           "/etc/machine-id"
         ];
-        users.${hosts.username}.directories = ["."];
       };
     };
-    system.activationScripts.persistent-dirs.text = (
-      let
-        mkHomePersist = user:
-          lib.optionalString user.createHome ''
-            mkdir -p /persist/${user.home}
-            chown ${user.name}:${user.group} /persist/${user.home}
-            chmod ${user.homeMode} /persist/${user.home}
-          '';
-        users = lib.attrValues config.users.users;
-      in
-        lib.concatLines (map mkHomePersist users)
-    );
   };
 }
