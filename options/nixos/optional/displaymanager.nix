a0ecd41c61c447ec5c87e13a30908dfb256966f0
bossearch <yudha.bosse@gmail.com>
2026-03-12 22:57:27 +0800

    fix: implement dual greetd/getty autologin with UWSM

--- options/nixos/optional/displaymanager.nix
+++ options/nixos/optional/displaymanager.nix
@@ -1,28 +1,76 @@
 {
   hosts,
   pkgs,
+  lib,
   ...
 }: let
-  fallbackScript = pkgs.writeShellScript "start-desktop" ''
-    # Make sure shell itself doesn't print anything either
-    exec 1>/dev/null 2>&1
+  autoLoginCmd = "${pkgs.uwsm}/bin/uwsm start hyprland-uwsm.desktop >/dev/null 2>&1";
 
-    if command -v Hyprland >/dev/null 2>&1; then
-      exec uwsm start hyprland-uwsm.desktop
-    else
-      chvt 2
-    fi
+  isGreetd = hosts.gui.enable && hosts.gui.displaymanager == "greetd";
+  isGetty = hosts.gui.enable && hosts.gui.displaymanager == "none";
+
+  ttyCheck =
+    if hosts.shell == "fish"
+    then ''
+      if test (tty) = "/dev/tty1"
+    ''
+    else ''
+      if [ "$(tty)" = "/dev/tty1" ]; then
+    '';
+
+  ttyEnd =
+    if hosts.shell == "fish"
+    then "end"
+    else "fi";
+
+  shellCmd = ''
+    ${ttyCheck}
+      exec ${autoLoginCmd}
+    ${ttyEnd}
   '';
 in {
+  # Greetd
   services.greetd = {
-    enable = hosts.gui.displaymanager == "greetd";
-    settings = rec {
+    enable = isGreetd;
+    settings = {
       initial_session = {
-        command = fallbackScript;
+        command = autoLoginCmd;
         user = hosts.username;
       };
-      default_session = initial_session;
+      default_session = {
+        command = autoLoginCmd;
+        user = hosts.username;
+      };
     };
   };
-  systemd.services."getty@tty2".enable = hosts.gui.displaymanager == "greetd";
+
+  # Getty
+  services.getty = lib.mkIf isGetty {
+    autologinUser = hosts.username;
+  };
+
+  programs = {
+    bash.loginShellInit = lib.mkIf (isGetty && hosts.shell == "bash") shellCmd;
+    fish.loginShellInit = lib.mkIf (isGetty && hosts.shell == "fish") shellCmd;
+    zsh.loginShellInit = lib.mkIf (isGetty && hosts.shell == "zsh") shellCmd;
+  };
+
+  systemd.services."getty@tty1" = lib.mkIf isGetty (lib.mkMerge [
+    {
+      serviceConfig = {
+        TTYVTDisallocate = "yes";
+        TTYReset = "yes";
+      };
+    }
+    (lib.mkIf hosts.silentboot {
+      overrideStrategy = "asDropin";
+      serviceConfig = {
+        ExecStart = [
+          ""
+          "@${pkgs.util-linux}/bin/agetty agetty --login-program ${pkgs.shadow}/bin/login --login-options '-f ${hosts.username}' --autologin ${hosts.username} --noissue --skip-login --noclear %I $TERM"
+        ];
+        StandardOutput = "null";
+      };
+    })
+  ]);
 }
