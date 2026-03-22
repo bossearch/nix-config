a68c5621d18b0aacdc2a2a1c3dc19ef796dd79bd
bossearch <yudha.bosse@gmail.com>
2026-03-23 02:38:15 +0800

    fix: ignore hidden nix files in autoimport lib

--- libs/autoimport.nix
+++ libs/autoimport.nix
@@ -4,8 +4,14 @@
       acc: name: type:
         acc
         ++ lib.optional (
-          (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
-          || (type == "directory" && !(lib.hasPrefix "." name))
+          (type
+            == "regular"
+            && lib.hasSuffix ".nix" name
+            && name != "default.nix"
+            && !(lib.hasPrefix "." name))
+          || (type
+            == "directory"
+            && !(lib.hasPrefix "." name))
         ) (path + "/${name}")
     ) [] (builtins.readDir path);
 }
