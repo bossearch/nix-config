6a12ddc331881c22b2e34ae88132f3682e7ea4b5
bossearch <yudha.bosse@gmail.com>
2026-04-04 16:03:09 +0800

    feat: update mythemux and refactor tmux theme schema

--- options/home-manager/core/cli/tmux/mythemux.nix
+++ options/home-manager/core/cli/tmux/mythemux.nix
@@ -3,8 +3,8 @@
     source = pkgs.fetchFromGitHub {
       owner = "bossearch";
       repo = "mythemux";
-      rev = "4250787c7ffc407134c4c0d3111479f8cede674d";
-      sha256 = "sha256-JZ/OF7VGKHyMhChKi7+SGYQvszlHricAK5qJQUl8VYY=";
+      rev = "46c4e3ebb302010cb1f257a3579c0be6bd9058b9";
+      sha256 = "sha256-FpIJexwnNl/q/5aPiYuQCCQDv+yMAaHhIGO5tuvlNLs=";
     };
     target = ".config/tmux/plugins/mythemux";
   };
--- options/home-manager/core/cli/tmux/theme.nix
+++ options/home-manager/core/cli/tmux/theme.nix
@@ -3,13 +3,16 @@
   base01 = "#${config.colorScheme.palette.base01}";
   base02 = "#${config.colorScheme.palette.base02}";
   base03 = "#${config.colorScheme.palette.base03}";
+  base04 = "#${config.colorScheme.palette.base04}";
   base05 = "#${config.colorScheme.palette.base05}";
   base07 = "#${config.colorScheme.palette.base07}";
   base08 = "#${config.colorScheme.palette.base08}";
+  base09 = "#${config.colorScheme.palette.base09}";
   base0A = "#${config.colorScheme.palette.base0A}";
   base0B = "#${config.colorScheme.palette.base0B}";
   base0C = "#${config.colorScheme.palette.base0C}";
   base0D = "#${config.colorScheme.palette.base0D}";
+  base0E = "#${config.colorScheme.palette.base0E}";
   base0F = "#${config.colorScheme.palette.base0F}";
 in {
   home.file.".config/tmux/theme.sh" = {
@@ -18,23 +21,24 @@ in {
       #!/usr/bin/env bash
 
       declare -A THEME=(
-        ["background"]="${base01}"
-        ["foreground"]="${base05}"
-        ["black"]="${base00}"
-        ["blue"]="${base0D}"
-        ["green"]="${base0B}"
-        ["white"]="${base07}"
-        ["bblack"]="${base03}"
-        ["bblue"]="${base0D}"
-        ["bgreen"]="${base0F}"
-        ['ghgreen']="${base0C}"
-        ['ghred']="${base08}"
-        ['ghyellow']="${base0A}"
-        ['ghsky']="${base02}"
-        ['ghturquoise']="${base0F}"
+        ["base00"]="${base00}"
+        ["base01"]="${base01}"
+        ["base02"]="${base02}"
+        ["base03"]="${base03}"
+        ["base04"]="${base04}"
+        ["base05"]="${base05}"
+        ["base07"]="${base07}"
+        ["base08"]="${base08}"
+        ["base09"]="${base09}"
+        ["base0A"]="${base0A}"
+        ["base0B"]="${base0B}"
+        ["base0C"]="${base0C}"
+        ["base0D"]="${base0D}"
+        ["base0E"]="${base0E}"
+        ["base0F"]="${base0F}"
       )
 
-      RESET="#[fg=''${THEME[foreground]},bg=''${THEME[background]},nobold,noitalics,nounderscore,nodim]"
+      RESET="#[fg=''${THEME[base05]},bg=''${THEME[base01]},nobold,noitalics,nounderscore,nodim]"
     '';
   };
 }
