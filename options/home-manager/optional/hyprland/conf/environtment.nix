5596f024a37a7e52560610bc561732304136da63
bossearch <yudha.bosse@gmail.com>
2026-05-03 19:56:02 +0800

    fix: clean up hyprland environment variables

--- options/home-manager/optional/hyprland/conf/environtment.nix
+++ options/home-manager/optional/hyprland/conf/environtment.nix
@@ -6,18 +6,13 @@
 
     # See https://wiki.hypr.land/Configuring/Environment-variables/
 
-    env = QT_QPA_PLATFORM,wayland;xcb
-    env = QT_QPA_PLATFORMTHEME,qt5ct
-    env = QT_STYLE_OVERRIDE,kvantum
-    env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-
-    env = XDG_SESSION_TYPE,wayland
-    env = SDL_VIDEODRIVER,wayland
     env = XDG_CURRENT_DESKTOP,Hyprland
     env = XDG_SESSION_DESKTOP,Hyprland
+    env = XDG_SESSION_TYPE,wayland
 
     env = GDK_BACKEND,wayland
-    env = WLR_BACKEND,vulkan
+    env = QT_QPA_PLATFORM,wayland;xcb
+    env = SDL_VIDEODRIVER,wayland
 
     env = MOZ_ENABLE_WAYLAND,1
     env = WLR_NO_HARDWARE_CURSORS,1
