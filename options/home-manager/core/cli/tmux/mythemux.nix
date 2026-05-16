fd90e2e8dded281a7b895099a22dfea2806fe253
bossearch <yudha.bosse@gmail.com>
2026-05-16 08:14:31 +0800

    fix: move mythemux target path to home.file attribute

--- options/home-manager/core/cli/tmux/mythemux.nix
+++ options/home-manager/core/cli/tmux/mythemux.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: {
-  home.file."mythemux" = {
+  home.file.".config/tmux/plugins/mythemux" = {
     source = pkgs.fetchFromGitHub {
       owner = "bossearch";
       repo = "mythemux";
       rev = "46c4e3ebb302010cb1f257a3579c0be6bd9058b9";
       sha256 = "sha256-FpIJexwnNl/q/5aPiYuQCCQDv+yMAaHhIGO5tuvlNLs=";
     };
-    target = ".config/tmux/plugins/mythemux";
   };
 }
