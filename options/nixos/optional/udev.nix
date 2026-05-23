fc286b511cba0307f1978da63a49d2c8cf9292ee
bossearch <yudha.bosse@gmail.com>
2026-05-23 21:47:57 +0800

    feat: migrate silvia usbguard & udev rules to secrets

--- options/nixos/optional/udev.nix
+++ options/nixos/optional/udev.nix
@@ -1,4 +1,5 @@
 {
+  config,
   hosts,
   lib,
   pkgs,
@@ -16,6 +17,29 @@
       ${pkgs.systemd}/bin/systemctl --user start "usbguard-prompt@$ESCAPED_PATH.service"
   '';
 in {
+  sops = lib.mkIf (hosts.gui.enable && hosts.usbguard && hosts.hostname == "silvia") {
+    secrets.udev1 = {};
+    secrets.udev2 = {};
+    templates."udevRules" = {
+      path = "/run/udev/rules.d/99-secrets.rules";
+      owner = "root";
+      group = "root";
+      mode = "0400";
+      content = ''
+        ACTION=="add", SUBSYSTEM=="usb", ENV{DEVNAME}!="", GOTO="run_usbguard"
+        GOTO="skip_usbguard"
+        LABEL="run_usbguard"
+
+        ${config.sops.placeholder.udev1}
+
+        RUN+="${usbguard} $env{DEVNAME}"
+        LABEL="skip_usbguard"
+
+        ${config.sops.placeholder.udev2}
+      '';
+    };
+  };
+
   services.udev = {
     enable = true;
     packages =
@@ -30,33 +54,12 @@ in {
 
     extraRules = lib.concatStringsSep "\n" (
       lib.flatten [
-        (lib.optional (hosts.gui.enable && hosts.usbguard && hosts.hostname == "silvia") ''
-          ACTION=="add", SUBSYSTEM=="usb", ENV{DEVNAME}!="", GOTO="run_usbguard"
-          GOTO="skip_usbguard"
-          LABEL="run_usbguard"
-
-          # usbguard rules for mouse, keyboard, usbhub, and usbhub monitor
-          ENV{ID_VENDOR_ID}=="1b1c", ENV{ID_MODEL_ID}=="1b3e", GOTO="skip_usbguard"
-          ENV{ID_VENDOR_ID}=="4653", ENV{ID_MODEL_ID}=="0001", GOTO="skip_usbguard"
-          ENV{ID_VENDOR_ID}=="2109", ENV{ID_MODEL_ID}=="2815", GOTO="skip_usbguard"
-          ENV{ID_VENDOR_ID}=="05e3", ENV{ID_MODEL_ID}=="0610", GOTO="skip_usbguard"
-
-          RUN+="${usbguard} $env{DEVNAME}"
-          LABEL="skip_usbguard"
-        '')
-
         (lib.optional (hosts.gui.enable && hosts.usbguard && hosts.hostname == "stagea") ''
           ACTION=="add", SUBSYSTEM=="usb", ENV{DEVNAME}!="", GOTO="run_usbguard"
           GOTO="skip_usbguard"
           LABEL="run_usbguard"
           RUN+="${usbguard} $env{DEVNAME}"
           LABEL="skip_usbguard"
-        '')
-
-        (lib.optional (hosts.gui.enable && hosts.udevqmk && hosts.hostname == "silvia") ''
-          # raw-hid for corne
-          SUBSYSTEM=="usb", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0001", MODE="0666", TAG+="uaccess"
-          KERNEL=="hidraw*", ATTRS{idVendor}== "4653", ATTRS{idProduct}=="0001", MODE="0666", TAG+="uaccess"
         '')
       ]
     );
--- options/nixos/optional/usbguard.nix
+++ options/nixos/optional/usbguard.nix
@@ -1,4 +1,5 @@
 {
+  config,
   hosts,
   lib,
   pkgs,
@@ -37,40 +38,34 @@ in {
       usbguard
     ];
 
-    services.usbguard = {
-      enable = true;
-      dbus.enable = true;
-      IPCAllowedUsers = ["root" "${hosts.username}"];
+    sops = lib.mkIf (hosts.hostname == "silvia") {
+      secrets.usbguard = {
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+    };
 
-      presentDevicePolicy = "apply-policy"; # Allow existing devices
-      presentControllerPolicy = "apply-policy"; # Allow existing hub
-      insertedDevicePolicy = "apply-policy"; # New devices require approval
-
-      rules = lib.mkMerge [
-        (lib.mkIf (hosts.hostname == "silvia") ''
-          allow id 1d6b:0002 serial "0000:08:00.3" with-interface 09:00:00
-          allow id 1d6b:0003 serial "0000:08:00.3" with-interface 09:00:00
-          allow id 1d6b:0002 serial "0000:08:00.4" with-interface 09:00:00
-          allow id 1d6b:0003 serial "0000:08:00.4" with-interface 09:00:00
-          allow id 1d6b:0002 serial "0000:09:00.0" with-interface 09:00:00
-
-          allow id 1d6b:0003 serial "0000:09:00.0" with-interface 09:00:00
-          allow id 0bda:5411 with-interface { 09:00:01 09:00:02 }
-          allow id 0bda:0411 with-interface 09:00:00
-          allow id 05e3:0610 with-interface { 09:00:01 09:00:02 }
-          allow id 8087:0aa7 with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 }
-
-          allow id 2109:2815 with-interface { 09:00:01 09:00:02 }
-          allow id 4653:0001
-          allow id 1b1c:1b3e serial "0A026035AF359CA25B23342BF5001BC6" with-interface { 03:01:02 03:00:00 }
-        '')
-        (lib.mkIf (hosts.hostname == "stagea") ''
+    services.usbguard = lib.mkMerge [
+      {
+        enable = true;
+        dbus.enable = true;
+        IPCAllowedUsers = ["root" "${hosts.username}"];
+        insertedDevicePolicy = "apply-policy";
+        presentControllerPolicy = "apply-policy";
+        presentDevicePolicy = "apply-policy";
+      }
+      (lib.mkIf (hosts.hostname == "silvia") {
+        ruleFile = config.sops.secrets.usbguard.path;
+      })
+      (lib.mkIf (hosts.hostname == "stagea") {
+        rules = ''
           allow id 1d6b:0002 serial "0000:02:00.0" with-interface 09:00:00
           allow id 1d6b:0003 serial "0000:02:00.0" with-interface 09:00:00
           allow id 0627:0001 serial "28754-0000:00:02.1:00.0-1" with-interface 03:00:00
-        '')
-      ];
-    };
+        '';
+      })
+    ];
     systemd.user.services."usbguard-prompt@" = lib.mkIf hosts.gui.enable {
       description = "USBGuard User Prompt for %i";
       serviceConfig = {
