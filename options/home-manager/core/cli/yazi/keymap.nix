e0c826635c57be68e417828ddb3749ae88295edf
bossearch <yudha.bosse@gmail.com>
2026-05-27 15:44:35 +0800

    fix: yazi directories keybinds to work with new repo

--- options/home-manager/core/cli/yazi/keymap.nix
+++ options/home-manager/core/cli/yazi/keymap.nix
@@ -72,104 +72,69 @@
         desc = "Go ~/Videos";
       }
       {
-        run = "cd ~/.local/share/Vaults";
-        on = ["g" "V"];
-        desc = "Go to vaults";
-      }
-      {
         run = "cd ~/.local/share/Trash/files";
         on = ["g" "t"];
         desc = "Go to trash";
       }
       {
         run = "cd ~/.nix-config/hosts";
-        on = ["g" "N" "."];
+        on = ["g" "n" "n"];
         desc = "nixconfig/hosts";
       }
       {
-        run = "cd ~/.nix-config/hosts/common/core";
-        on = ["g" "N" "c"];
-        desc = "nixconfig/hosts.../core";
+        run = "cd ~/.nix-config/homes";
+        on = ["g" "n" "h"];
+        desc = "nixconfig/homes";
       }
       {
-        run = "cd ~/.nix-config/hosts/common/optional";
-        on = ["g" "N" "o"];
-        desc = "nixconfig/hosts.../optional";
+        run = "cd ~/.nix-config/libs";
+        on = ["g" "n" "l"];
+        desc = "nixconfig/libs";
       }
       {
-        run = "cd ~/.nix-config/hosts/droid";
-        on = ["g" "N" "d"];
-        desc = "nixconfig/hosts.../droid";
+        run = "cd ~/.nix-config/modules/nixos";
+        on = ["g" "n" "m" "n"];
+        desc = "nixconfig/modules/nixos";
       }
       {
-        run = "cd ~/.nix-config/modules";
-        on = ["g" "N" "m"];
-        desc = "nixconfig/modules";
+        run = "cd ~/.nix-config/modules/home-manager";
+        on = ["g" "n" "m" "h"];
+        desc = "nixconfig/modules/home-manager";
+      }
+      {
+        run = "cd ~/.nix-config/options/nixos/core";
+        on = ["g" "n" "o" "n" "c"];
+        desc = "nixconfig/options/nixos/core";
+      }
+      {
+        run = "cd ~/.nix-config/options/nixos/optional";
+        on = ["g" "n" "o" "n" "o"];
+        desc = "nixconfig/options/nixos/optional";
+      }
+      {
+        run = "cd ~/.nix-config/options/home-manager/core";
+        on = ["g" "n" "o" "h" "c"];
+        desc = "nixconfig/options/home-manager/core";
+      }
+      {
+        run = "cd ~/.nix-config/options/home-manager/optional";
+        on = ["g" "n" "o" "h" "o"];
+        desc = "nixconfig/options/home-manager/optional";
       }
       {
         run = "cd ~/.nix-config/overlays";
-        on = ["g" "N" "O"];
+        on = ["g" "n" "O"];
         desc = "nixconfig/overlays";
       }
       {
         run = "cd ~/.nix-config/pkgs";
-        on = ["g" "N" "p"];
+        on = ["g" "n" "p"];
         desc = "nixconfig/pkgs";
       }
       {
-        run = "cd ~/.nix-config/home";
-        on = ["g" "n" "."];
-        desc = "nixconfig/home";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core";
-        on = ["g" "n" "c"];
-        desc = "nixconfig/home.../core";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core/nvim";
-        on = ["g" "n" "n"];
-        desc = "nixconfig/home.../neovim";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core/cli/tmux";
+        run = "cd ~/.nix-config/themes";
         on = ["g" "n" "t"];
-        desc = "nixconfig/home.../tmux";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core/cli/yazi";
-        on = ["g" "n" "y"];
-        desc = "nixconfig/home.../yazi";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core/cli/fzf";
-        on = ["g" "n" "f"];
-        desc = "nixconfig/home.../fzf";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/core/shell";
-        on = ["g" "n" "s"];
-        desc = "nixconfig/home.../shell";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/optional";
-        on = ["g" "n" "o"];
-        desc = "nixconfig/home.../optional";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/optional/hyprland";
-        on = ["g" "n" "h"];
-        desc = "nixconfig/home.../hyprland";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/optional/mpv";
-        on = ["g" "n" "m"];
-        desc = "nixconfig/home.../mpv";
-      }
-      {
-        run = "cd ~/.nix-config/home/common/optional/waybar";
-        on = ["g" "n" "w"];
-        desc = "nixconfig/home.../waybar";
+        desc = "nixconfig/themes";
       }
       {
         on = ["c" "d"];
