8da70f5ec467e8c2f663262b3a7d244b10698fce
bossearch <yudha.bosse@gmail.com>
2026-06-18 13:55:36 +0800

    refactor(boot): keep hardware-configuration vanilla - add boot.systemd.enable = false only for temporary

--- hosts/silvia/hardware-configuration.nix
+++ hosts/silvia/hardware-configuration.nix
@@ -12,8 +12,8 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["ahci" "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["dm-snapshot"];
+  boot.initrd.availableKernelModules = ["ahci" "nvme" "xhci_pci" "usbhid"];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
--- options/nixos/core/boot.nix
+++ options/nixos/core/boot.nix
@@ -1,8 +1,19 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   boot = {
     loader.systemd-boot.enable = true;
     loader.systemd-boot.consoleMode = "max";
     loader.efi.canTouchEfiVariables = true;
     loader.timeout = 1;
+    kernelPackages = pkgs.linuxPackages_zen;
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
   };
 }
