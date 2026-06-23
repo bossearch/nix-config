06b0ada3007214a6e92b260c755cfe1c56e74b6b
bossearch <yudha.bosse@gmail.com>
2026-06-24 00:33:36 +0800

    fix(hyprland): add autostart to copy wallpaper for retroarch

--- options/home-manager/optional/hyprland/conf/autostart.nix
+++ options/home-manager/optional/hyprland/conf/autostart.nix
@@ -1,15 +1,20 @@
 {
   config,
   homes,
+  hosts,
   ...
 }: let
+  footclient =
+    if config.programs.foot.enable
+    then "exec-once = ~/.config/foot/footclient.sh"
+    else "";
   gowall =
     if homes.gowall
     then "exec-once = ~/.config/gowall/daily-wallpaper.sh"
     else "";
-  footclient =
-    if config.programs.foot.enable
-    then "exec-once = ~/.config/foot/footclient.sh"
+  retroarch =
+    if homes.game.retroarch
+    then "exec-once = cp ~/.cache/${hosts.username}/hyprpaper.png ~/.cache/${hosts.username}/retroarch.png"
     else "";
 in {
   wayland.windowManager.hyprland.extraConfig = ''
@@ -25,7 +30,8 @@ in {
     exec-once = wl-paste --watch cliphist store
 
     exec-once = ~/.config/hypr/scripts/startup-value.sh
-    ${gowall}
     ${footclient}
+    ${gowall}
+    ${retroarch}
   '';
 }
