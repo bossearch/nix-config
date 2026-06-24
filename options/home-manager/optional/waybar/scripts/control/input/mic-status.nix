73597a28a7ca71febd2b26784d87bcc74cacf15b
bossearch <yudha.bosse@gmail.com>
2026-06-25 05:57:13 +0800

    feat(waybar): add host-specific handling to mic-status script

--- options/home-manager/optional/waybar/scripts/control/input/mic-status.nix
+++ options/home-manager/optional/waybar/scripts/control/input/mic-status.nix
@@ -1,16 +1,12 @@
 {
   homes,
+  hosts,
   lib,
   ...
-}: {
-  home.file.".config/waybar/scripts/control/input/mic-status.sh" = lib.mkIf homes.waybar {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-
-      CURRENT_SOURCE=$(pactl get-default-source)
-      MUTE_STATE=$(pactl get-source-mute $CURRENT_SOURCE | awk '{print $2}')
-
+}: let
+  source =
+    if hosts.hostname == "silvia"
+    then ''
       DESK_MIC="alsa_input.pci-0000_08_00.6.analog-stereo"
       WEBCAM_MIC="alsa_input.usb-046d_Brio_100_2603AP74QPC8-02.mono-fallback"
       TWS_MIC="bluez_input.00:A4:1C:F9:15:84"
@@ -27,7 +23,36 @@
         echo "{\"text\": \"󰂯\"}"
       elif [[ "$CURRENT_SOURCE" == "$TWS_MIC" && "$MUTE_STATE" == "yes" ]]; then
         echo "{\"text\": \"󰂯\"}"
+      else
+        echo "{\"text\": \"?\"}"
       fi
+    ''
+    else ''
+      if [[ "$MUTE_STATE" == "no" ]]; then
+        echo "{\"text\": \"\"}"
+      if [[ "$MUTE_STATE" == "yes" ]]; then
+        echo "{\"text\": \"\"}"
+      else
+        echo "{\"text\": \"?\"}"
+      fi
+    '';
+in {
+  home.file.".config/waybar/scripts/control/input/mic-status.sh" = lib.mkIf homes.waybar {
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+
+      for i in {1..10}; do
+        if pactl info &>/dev/null; then
+          break
+        fi
+        sleep 1
+      done
+
+      CURRENT_SOURCE=$(pactl get-default-source)
+      MUTE_STATE=$(pactl get-source-mute $CURRENT_SOURCE | awk '{print $2}')
+
+      ${source}
     '';
   };
 }
