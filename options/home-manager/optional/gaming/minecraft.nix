7dd5e6ef000c102096ddd4c3050897e9d1271817
bossearch <yudha.bosse@gmail.com>
2026-06-25 02:26:27 +0800

    refactor(minecraft): switch prismlauncher to program module

--- options/home-manager/optional/gaming/minecraft.nix
+++ options/home-manager/optional/gaming/minecraft.nix
@@ -4,24 +4,22 @@
   pkgs,
   ...
 }: {
-  home.packages = lib.mkIf homes.game.prismlauncher [
-    (pkgs.prismlauncher.override {
+  programs.prismlauncher = lib.mkIf homes.game.prismlauncher {
+    enable = true;
+    package = pkgs.prismlauncher.override {
       jdks = [
         pkgs.jre21_minimal
         pkgs.jre25_minimal
       ];
-      additionalLibs = with pkgs;
-        [
-          wayland
-        ]
-        ++ (with pkgs; [
-          libXcursor
-          libXrandr
-          libXi
-          libXinerama
-          libXfixes
-          libXrender
-        ]);
-    })
-  ];
+      additionalLibs = with pkgs; [
+        wayland
+        libXcursor
+        libXrandr
+        libXi
+        libXinerama
+        libXfixes
+        libXrender
+      ];
+    };
+  };
 }
