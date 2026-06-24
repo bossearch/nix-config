429adc842ad3ee92d570e7ff29111f5229b1bf41
bossearch <yudha.bosse@gmail.com>
2026-06-25 01:58:38 +0800

    feat(hyprland): handle player switching and toggle edge cases

--- options/home-manager/optional/hyprland/scripts/playerctl/default.nix
+++ options/home-manager/optional/hyprland/scripts/playerctl/default.nix
@@ -23,40 +23,59 @@ in {
       PLAYER_FILE="$HOME/.cache/${hosts.username}/player"
       mapfile -t AVAILABLE_PLAYERS < <(playerctl -l 2>/dev/null)
 
+      set_player() {
+        echo -n "$NEXT_PLAYER" >"$PLAYER_FILE"
+        STATUS=$(playerctl -p "$NEXT_PLAYER" status 2>/dev/null)
+        if [ "$STATUS" == "Playing" ]; then
+          ICON="media-playback-start"
+        else
+          ICON="media-playback-pause"
+        fi
+        ${close}
+      }
+
+      swap_player() {
+        if [ -f "$PLAYER_FILE" ]; then
+          CURRENT_PLAYER=$(tr -d '\n' <"$PLAYER_FILE" | xargs)
+        fi
+
+        if [[ -z "$CURRENT_PLAYER" ]] || [[ ! " ''${AVAILABLE_PLAYERS[*]} " =~ $CURRENT_PLAYER ]]; then
+          CURRENT_PLAYER="''${AVAILABLE_PLAYERS[0]}"
+        fi
+
+        NEXT_PLAYER=""
+        for i in "''${!AVAILABLE_PLAYERS[@]}"; do
+          if [[ "''${AVAILABLE_PLAYERS[$i]}" == "$CURRENT_PLAYER" ]]; then
+            NEXT_INDEX=$(((i + 1) % ''${#AVAILABLE_PLAYERS[@]}))
+            NEXT_PLAYER="''${AVAILABLE_PLAYERS[$NEXT_INDEX]}"
+            break
+          fi
+        done
+
+        NEXT_PLAYER="''${NEXT_PLAYER:-''${AVAILABLE_PLAYERS[0]}}"
+      }
+
       if [ "''${#AVAILABLE_PLAYERS[@]}" -eq 0 ]; then
+        ${close}
         notify-send -e -a playerctl "Playerctl" "No media players found" -i dialog-warning
         exit 0
-      fi
-
-      if [ -f "$PLAYER_FILE" ]; then
-        CURRENT_PLAYER=$(tr -d '\n' <"$PLAYER_FILE" | xargs)
-      fi
-
-      if [[ -z "$CURRENT_PLAYER" ]] || [[ ! " ''${AVAILABLE_PLAYERS[*]} " =~ $CURRENT_PLAYER ]]; then
-        CURRENT_PLAYER="''${AVAILABLE_PLAYERS[0]}"
-      fi
-
-      NEXT_PLAYER=""
-      for i in "''${!AVAILABLE_PLAYERS[@]}"; do
-        if [[ "''${AVAILABLE_PLAYERS[$i]}" == "$CURRENT_PLAYER" ]]; then
-          NEXT_INDEX=$(((i + 1) % ''${#AVAILABLE_PLAYERS[@]}))
-          NEXT_PLAYER="''${AVAILABLE_PLAYERS[$NEXT_INDEX]}"
-          break
+      elif [ "''${#AVAILABLE_PLAYERS[@]}" -eq 1 ]; then
+        if [ "''${AVAILABLE_PLAYERS[0]}" != "$PLAYER_FILE" ]; then
+          swap_player
+          set_player
+          if [ -z "$1" ]; then
+            notify-send -e -a playerctl "Playerctl" "Only $NEXT_PLAYER found" -i "$ICON"
+          fi
+        else
+          NEXT_PLAYER="''${AVAILABLE_PLAYERS[0]}"
+          set_player
+          notify-send -e -a playerctl "Playerctl" "Only $NEXT_PLAYER found" -i "$ICON"
         fi
-      done
-
-      NEXT_PLAYER="''${NEXT_PLAYER:-''${AVAILABLE_PLAYERS[0]}}"
-      echo -n "$NEXT_PLAYER" >"$PLAYER_FILE"
-
-      STATUS=$(playerctl -p "$NEXT_PLAYER" status 2>/dev/null)
-      if [ "$STATUS" == "Playing" ]; then
-        ICON="media-playback-start"
       else
-        ICON="media-playback-pause"
+        swap_player
+        set_player
+        notify-send -e -a playerctl "Playerctl" "$NEXT_PLAYER - $STATUS" -i "$ICON"
       fi
-
-      ${close}
-      notify-send -e -a playerctl "$NEXT_PLAYER" "$STATUS" -i "$ICON"
     '';
   };
 }
--- options/home-manager/optional/hyprland/scripts/playerctl/player-toggle.nix
+++ options/home-manager/optional/hyprland/scripts/playerctl/player-toggle.nix
@@ -18,19 +18,40 @@ in {
       #!/usr/bin/env bash
 
       PLAYER_FILE="$HOME/.cache/${hosts.username}/player"
-      PLAYER="$(cat "$PLAYER_FILE")"
+      mapfile -t AVAILABLE_PLAYERS < <(playerctl -l 2>/dev/null)
 
-      playerctl -p "$PLAYER" play-pause
+      toggle_player() {
+        playerctl -p "$NEXT_PLAYER" play-pause
+        sleep 0.001
 
-      STATUS=$(playerctl -p "$PLAYER" status 2>/dev/null)
-      if [ "$STATUS" == "Playing" ]; then
-        ICON="media-playback-start"
+        STATUS=$(playerctl -p "$NEXT_PLAYER" status 2>/dev/null)
+        if [ "$STATUS" == "Playing" ]; then
+          ICON="media-playback-start"
+        else
+          ICON="media-playback-pause"
+        fi
+        ${close}
+      }
+
+      if [ "''${#AVAILABLE_PLAYERS[@]}" -eq 0 ]; then
+        ${close}
+        notify-send -e -a playerctl "Playerctl" "No media players found" -i dialog-warning
+        exit 0
+      elif [ "''${#AVAILABLE_PLAYERS[@]}" -eq 1 ]; then
+        if [ "''${AVAILABLE_PLAYERS[0]}" != "$(cat "$PLAYER_FILE")" ]; then
+          "$HOME/.config/hypr/scripts/playerctl/playerctl.sh silent"
+          toggle_player
+          notify-send -e -a playerctl "Playerctl" "''${AVAILABLE_PLAYERS[0]} - $STATUS" -i "$ICON"
+        else
+          NEXT_PLAYER="''${AVAILABLE_PLAYERS[0]}"
+          toggle_player
+          notify-send -e -a playerctl "Playerctl" "$NEXT_PLAYER - $STATUS" -i "$ICON"
+        fi
       else
-        ICON="media-playback-pause"
+        NEXT_PLAYER="$(cat "$PLAYER_FILE")"
+        toggle_player
+        notify-send -e -a playerctl "Playerctl" "$NEXT_PLAYER - $STATUS" -i "$ICON"
       fi
-
-      ${close}
-      notify-send -e -a playerctl "$PLAYER" "$STATUS" -i "$ICON"
     '';
   };
 }
