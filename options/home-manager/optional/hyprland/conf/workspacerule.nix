79f4e3c6afe438ab5862ee63833686583c87285d
bossearch <yudha.bosse@gmail.com>
2026-07-27 22:30:36 +0800

    refactor(hyprland): toggle gamemode via lua workspace events

--- options/home-manager/optional/hyprland/conf/workspacerule.nix
+++ options/home-manager/optional/hyprland/conf/workspacerule.nix
@@ -1,4 +1,11 @@
-{
+{homes, ...}: let
+  close =
+    if homes.notify == "dunst"
+    then "hl.exec_cmd('dunstctl close-all')"
+    else if homes.notify == "swaync"
+    then "hl.exec_cmd('swaync-client --hide-all')"
+    else "";
+in {
   wayland.windowManager.hyprland.extraLuaFiles = {
     "lua.workspacerule" = {
       autoLoad = true;
@@ -12,6 +19,32 @@
         hl.workspace_rule({ workspace = "2", layout = "scrolling", on_created_empty = "firefox" })
         hl.workspace_rule({ workspace = "6", layout = "scrolling" })
         hl.workspace_rule({ workspace = "7", layout = "monocle" })
+
+        -- auto toggle gamemode
+        local gamemode_enabled = false
+        hl.on("workspace.active", function(ws)
+            if ws.id == 7 then
+                if not gamemode_enabled then
+                    hl.exec_cmd("~/.config/qmk/crkbd-toggle-game.py 1")
+                    require("lib.util").gamemode("enable")
+                    ${close}
+                    hl.exec_cmd('notify-send -e -a nosound -i state_running "Game Mode" "Activated"')
+                    gamemode_enabled = true
+                end
+            else
+                if gamemode_enabled then
+                    hl.exec_cmd("~/.config/qmk/crkbd-toggle-game.py 0")
+                    require("lib.util").gamemode("disable")
+                    ${close}
+                    hl.exec_cmd('notify-send -e -a nosound -i state_paused "Game Mode" "Deactivated"')
+                    gamemode_enabled = false
+
+                    hl.on("workspace.removed", function()
+                        require("lib.util").gamemode("vfr")
+                    end)
+                end
+            end
+        end)
       '';
     };
   };
--- options/home-manager/optional/hyprland/scripts/workspace.nix
+++ options/home-manager/optional/hyprland/scripts/workspace.nix
@@ -1,23 +1,14 @@
 {
-  homes,
   hosts,
   lib,
   ...
 }: let
   enabled = hosts.gui.enable && hosts.gui.windowmanager == "hyprland";
-  close =
-    if homes.notify == "dunst"
-    then "dunstctl close-all"
-    else if homes.notify == "swaync"
-    then "swaync-client --hide-all"
-    else "";
 in {
   home.file.".config/hypr/scripts/workspace.sh" = lib.mkIf enabled {
     executable = true;
     text = ''
       #!/usr/bin/env bash
-
-      OLD_WORKSPACE=$(hyprctl activeworkspace -j | jq -r '.id')
 
       if [[ "$1" == "focus" ]]; then
         if [[ "$2" == "next" ]]; then
@@ -34,39 +25,6 @@ in {
           hyprctl eval "require('lib.util').move_to_workspace('prev')"
         else
           hyprctl eval "require('lib.util').move_to_selected_workspace($2)"
-        fi
-      fi
-
-      NEW_WORKSPACE=$(hyprctl activeworkspace -j | jq -r '.id')
-
-      gamemode() {
-        local mode="$1"
-        if [[ $mode == "enable" ]]; then
-          ~/.config/qmk/crkbd-toggle-game.py 1
-          hyprctl eval "require('lib.util').enable_gamemode()"
-          ${close}
-          notify-send -e -a nosound -i state_running "Game Mode" "Activated"
-        elif [[ $mode == "disable" ]]; then
-          ~/.config/qmk/crkbd-toggle-game.py 0
-          hyprctl eval "require('lib.util').disable_gamemode()"
-          ${close}
-          notify-send -e -a nosound -i state_paused "Game Mode" "Deactivated"
-        fi
-      }
-
-      if [[ "$NEW_WORKSPACE" == "7" && "$OLD_WORKSPACE" != "7" ]]; then
-        gamemode enable
-      elif [[ "$NEW_WORKSPACE" != "7" && "$OLD_WORKSPACE" == "7" ]]; then
-        gamemode disable
-      else
-        :
-      fi
-
-      if hyprctl workspaces | grep -q "workspace ID 7"; then
-        :
-      else
-        if [ "$(hyprctl getoption debug:vfr -j | jq '.bool')" == "false" ]; then
-          hyprctl eval "hl.config({debug={vfr=true}})"
         fi
       fi
     '';
