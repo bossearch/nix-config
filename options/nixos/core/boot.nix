dd3f729dfca22e99c93151539a69ac231e175956
bossearch <yudha.bosse@gmail.com>
2026-07-30 18:55:46 +0800

    refactor(boot): make some boot options specific to host

--- options/nixos/core/boot.nix
+++ options/nixos/core/boot.nix
@@ -1,19 +1,27 @@
 {
   config,
   pkgs,
+  lib,
+  hosts,
   ...
 }: {
-  boot = {
-    loader.systemd-boot.enable = true;
-    loader.systemd-boot.consoleMode = "max";
-    loader.efi.canTouchEfiVariables = true;
-    loader.timeout = 1;
-    kernelPackages = pkgs.linuxPackages_zen;
-    extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback
-    ];
-    extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    '';
-  };
+  boot = lib.mkMerge [
+    {
+      loader = {
+        systemd-boot.enable = true;
+        systemd-boot.consoleMode = "max";
+        efi.canTouchEfiVariables = true;
+        timeout = 1;
+      };
+      kernelPackages = pkgs.linuxPackages_zen;
+    }
+    (lib.mkIf (hosts.hostname == "silvia") {
+      extraModulePackages = with config.boot.kernelPackages; [
+        v4l2loopback
+      ];
+      extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
+    })
+  ];
 }
