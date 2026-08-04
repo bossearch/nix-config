a1a06ce34031e98b233481dde3c0c2c927e0f144
bossearch <yudha.bosse@gmail.com>
2026-08-04 08:07:05 +0800

    refactor(gimp): use dynamic version for config dir path

--- options/home-manager/optional/gimp.nix
+++ options/home-manager/optional/gimp.nix
@@ -3,12 +3,14 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  gimpVersion = lib.versions.majorMinor pkgs.gimp.version;
+in {
   home = lib.mkIf homes.gimp {
     packages = with pkgs; [
       gimp-with-plugins
     ];
-    file.".config/GIMP/3.0/plug-ins/gimp-dds-plugin" = {
+    file.".config/GIMP/${gimpVersion}/plug-ins/gimp-dds-plugin" = {
       source = pkgs.fetchFromGitHub {
         owner = "gyoerkaa";
         repo = "gimp-dds-plugin";
@@ -16,14 +18,14 @@
         sha256 = "sha256-8jJHdR/hMCcYOxYMdU9/Ms+qsHxsnnDlwdnR8XCt0xA=";
       };
     };
-    file.".config/GIMP/3.0/controllerrc" = {
+    file.".config/GIMP/${gimpVersion}/controllerrc" = {
       source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/loloolooo/photoshop-keymap-for-gimp/refs/heads/3.x/controllerrc";
         sha256 = "sha256-EuIKudCzGcYCkT3kIG/Z+aUnV0Gk8cV30ZQ7JJAiUK8=";
       };
       force = true;
     };
-    file.".config/GIMP/3.0/shortcutsrc" = {
+    file.".config/GIMP/${gimpVersion}/shortcutsrc" = {
       source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/loloolooo/photoshop-keymap-for-gimp/refs/heads/3.x/shortcutsrc";
         sha256 = "sha256-yAa4ZOP8iSKIm6OPFRS7yr86r1WLvRcx9k0GGyNKVIE=";
