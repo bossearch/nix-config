70e8ba0d0b313a86963bd00db4b80af47e6499de
bossearch <yudha.bosse@gmail.com>
2026-08-04 08:05:01 +0800

    chore(pkgs): remove zsh-auto-notify custom package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   seniorpw = pkgs.callPackage ./seniorpw {};
   sysboard = pkgs.callPackage ./sysboard {};
-  zsh-auto-notify = pkgs.callPackage ./zsh-auto-notify {};
   # TODO: remove me after waybar 0.16.0
   waybar-fix-hypr-lua = pkgs.callPackage ./waybar-fix-hypr-lua {};
 }
