e0ebf5b5d50148d29e0c7da4afcac9b470463b27
bossearch <yudha.bosse@gmail.com>
2026-08-05 11:09:10 +0800

    refactor(fzf): use systemd-run for drun & run instead setsid

--- options/home-manager/core/cli/fzf/extra/drun.nix
+++ options/home-manager/core/cli/fzf/extra/drun.nix
@@ -4,22 +4,17 @@
     text = ''
       #!/usr/bin/env bash
 
-      # Read the app names and Exec commands from the saved txt file
       APPS_LIST=$(cat "$HOME/.cache/${hosts.username}/drun.txt")
 
-      # Use fzf to list app names and select multiple
       SELECTED=$(
         echo "$APPS_LIST" | cut -d'|' -f1 | sort |
           fzf -m --layout=default --border=none --bind 'tab:select+backward-kill-word'
       )
 
-      # Run each selected application's Exec command
       while IFS= read -r APP; do
         if [ -n "$APP" ]; then
           EXEC_COMMAND=$(echo "$APPS_LIST" | grep "^$APP|" | cut -d'|' -f2)
-          # read -r -a cmd_array <<<"$EXEC_COMMAND"
-          # setsid --fork "''${cmd_array[@]}" >/dev/null 2>&1 </dev/null
-          setsid --fork bash -c "$EXEC_COMMAND" >/dev/null 2>&1 </dev/null
+          systemd-run --user --no-block bash -c "$EXEC_COMMAND" >/dev/null 2>&1
         fi
       done <<<"$SELECTED"
 
--- options/home-manager/core/cli/fzf/extra/run.nix
+++ options/home-manager/core/cli/fzf/extra/run.nix
@@ -4,21 +4,15 @@
     text = ''
       #!/usr/bin/env bash
 
-      # List all executables from PATH
       IFS=':' read -r -a path_dirs <<< "$PATH"
       EXECUTABLES=$(find -L "''${path_dirs[@]}" -maxdepth 1 -type f -executable 2>/dev/null \
         | awk -F'/' '{print $NF}' | sort -u)
 
-      # Use fzf to select multiple commands
       SELECTED=$(echo "$EXECUTABLES" | fzf -m --layout=default --border=none --bind 'tab:select+backward-kill-word')
 
-      # Process each selected command
       while IFS= read -r COMMAND; do
         if [ -n "$COMMAND" ]; then
-          # Run the selected command
-
-          setsid bash -c "$COMMAND" >/dev/null 2>&1 & disown
-          # echo "$command" | xargs -r -I {} bash -c 'nohup {} >/dev/null 2>&1 & disown'
+          systemd-run --user --no-block bash -c "$COMMAND" >/dev/null 2>&1
         fi
       done <<< "$SELECTED"
 
