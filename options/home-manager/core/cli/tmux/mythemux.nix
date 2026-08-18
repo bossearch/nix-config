abbf5604c442c6cab716e92a5d37d495f139ca90
bossearch <yudha.bosse@gmail.com>
2026-08-18 21:34:16 +0800

    chore(tmux): update mythemux to latest commit

--- options/home-manager/core/cli/tmux/mythemux.nix
+++ options/home-manager/core/cli/tmux/mythemux.nix
@@ -3,8 +3,8 @@
     source = pkgs.fetchFromGitHub {
       owner = "bossearch";
       repo = "mythemux";
-      rev = "f1818515c77673aa160316217f150da58cc4b699";
-      sha256 = "sha256-8TRhyEOmtvpENc1CC7hu573qZwBYwRsKK2J4EBQeTMk=";
+      rev = "d52ad8c7ece60961644de8df0e9e2ec27fff93ec";
+      sha256 = "sha256-Oc5DsSyvnXg5RMqRUmREb5jFYfiNgMIbTtkSolgSSeE=";
     };
   };
 }
