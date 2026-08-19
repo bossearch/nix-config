ad20a5eb7c3583819bdf12752a3b9e33bab2c759
bossearch <yudha.bosse@gmail.com>
2026-08-20 00:03:26 +0800

    feat(sysboard): use built in sysboard auto hide

--- options/home-manager/optional/waybar/scripts/utility/virtualkeyboard.nix
+++ options/home-manager/optional/waybar/scripts/utility/virtualkeyboard.nix
@@ -8,11 +8,10 @@
     text = ''
       #!/usr/bin/env bash
 
-      if ! hyprctl layers | grep -q "namespace: sysboard"; then
+      if ! pgrep sysboard >/dev/null; then
         hyprctl eval "hl.exec_cmd('sysboard -H 400 -m 8')"
         sleep 0.1
         notify-send -e "Virtual Keyboard" "On" -i keyboard
-        pkill -RTMIN sysboard
       else
         notify-send -e 'Virtual Keyboard' 'Off' -i keyboard
         pkill sysboard
