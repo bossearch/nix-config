23fab0ae510fdbd931e7b9d23cf3fb5ed89e3b98
bossearch <yudha.bosse@gmail.com>
2026-08-21 20:54:03 +0800

    refactor(zk): apply title case formatting to note heading

--- options/home-manager/core/cli/zk/templates/main.nix
+++ options/home-manager/core/cli/zk/templates/main.nix
@@ -8,7 +8,7 @@
       tags: []
       ---
 
-      # {{title}}
+      # {{#sh "awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)}1' | tr -d '\n'"}}{{title}}{{/sh}}
 
       {{content}}
     '';
