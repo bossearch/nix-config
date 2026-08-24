7f939c0eaa77915bb56179b26eeb998849996d65
bossearch <yudha.bosse@gmail.com>
2026-08-24 23:53:43 +0800

    feat(hyprland): update hypridle to avoid multiple hyprlock instances

--- options/home-manager/optional/hyprland/hypridle.nix
+++ options/home-manager/optional/hyprland/hypridle.nix
@@ -1,30 +1,34 @@
-{hosts, ...}: let
-  enabled =
-    hosts.gui.enable
-    && hosts.hostname == "silvia";
+{
+  hosts,
+  pkgs,
+  ...
+}: let
+  enabled = hosts.gui.enable && hosts.hostname == "silvia";
 in {
   services.hypridle = {
     enable = enabled;
     settings = {
       general = {
-        after_sleep_cmd = "sleep 3 && systemctl --user restart waybar";
+        lock_cmd = "pidof hyprlock || ~/.config/hypr/scripts/hyprlock.sh";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
         # HYPRLOCK TIMEOUT
         {
           timeout = 900;
-          on-timeout = "(playerctl -a status | grep -q 'Playing' || ~/.config/hypr/scripts/hyprlock.sh)";
+          on-timeout = "loginctl lock-session";
         }
         # DPMS TIMEOUT
         {
           timeout = 1800;
-          on-timeout = "(playerctl -a status | grep -q 'Playing' || hyprctl dispatch 'hl.dsp.dpms({action = \"off\"})')";
+          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({action = \"off\"})'";
           on-resume = "hyprctl dispatch 'hl.dsp.dpms({action = \"on\"})'";
         }
         # SUSPEND TIMEOUT
         {
           timeout = 3600;
-          on-timeout = "(playerctl -a status | grep -q 'Playing' || systemctl suspend)";
+          on-timeout = "systemctl suspend";
         }
       ];
     };
