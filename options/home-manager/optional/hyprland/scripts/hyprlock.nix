8fd7f07cb0fb269e5bd648910a923f3179464ce1
bossearch <yudha.bosse@gmail.com>
2026-08-28 23:59:09 +0800

    refactor(hyprland): async wallpaper copy & simplify hyprlock script

--- options/home-manager/optional/hyprland/scripts/hyprlock.nix
+++ options/home-manager/optional/hyprland/scripts/hyprlock.nix
@@ -12,16 +12,14 @@ in {
 
       CACHE_DIR="$HOME/.cache/${hosts.username}"
       SCREENSHOT_PATH="$CACHE_DIR/hyprlock.png"
+
       rm "$SCREENSHOT_PATH"
+
       grim -l 0 -t png "$SCREENSHOT_PATH"
 
-      if pgrep -x senior-agent >/dev/null; then
-        pkill -x senior-agent
-      fi
-
       hyprlock &
-      sleep 5
-      cp "$CACHE_DIR/hyprpaper.png" "$CACHE_DIR/hyprlock.png"
+
+      (sleep 5 && cp "$CACHE_DIR/hyprpaper.png" "$CACHE_DIR/hyprlock.png") &
     '';
   };
 }
